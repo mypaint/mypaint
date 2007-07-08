c7c89145d8c0c592745c73b32169f833d6c23b91
Martin Renold <martinxyz@gmx.ch>
2007-07-08 09:16:45 +0000

    more gesture fine-tuning + logging
    svn://old.homeip.net/code/mypaint@525

--- gestures.c
+++ gestures.c
@@ -22,33 +22,47 @@ int detect_gestures (double dtime, double x, double y, double pressure)
   vx.time_constant = 0.05;
   vy.time_constant = 0.05;
   vtot.time_constant = 0.1;
-  slow_v.time_constant = 1.0;
+  slow_v.time_constant = 5.0;
   
   lfd_update(&vx, dtime, x);
   lfd_update(&vy, dtime, y);
   double v = hypot(vx.filtered_derivative, vy.filtered_derivative);
   lfd_update(&vtot, dtime, v);
-  lfd_update(&slow_v, dtime, v);
+
+  if (pressure > 0 && dtime < 0.1) {
+    lfd_update(&slow_v, dtime, v);
+  }
   //v = vtot.filtered_signal;
 
-  double v_ratio = v/(slow_v.filtered_signal+0.001);
+  double v_ratio = v/(slow_v.filtered_signal+0.1);
 
   static int state = 0;
   static double pressed_time;
   static double released_time;
-  static double max_v_ratio;
+  static double v_ratio_hist[30];
+  static int v_ratio_hist_n;
   double signal = 0;
   if (state == 0) {
     if (pressure > 0) {
       state = 1;
       pressed_time = time;
-      max_v_ratio = 0;
+      v_ratio_hist[0] = v_ratio;
+      v_ratio_hist_n = 1;
     }
   } else if (state == 1) {
-    int duration = time - pressed_time;
+    double duration = time - pressed_time;
+    int n = (int)(duration / 0.01);
+    while (n > v_ratio_hist_n-1 && v_ratio_hist_n < 30) {
+      v_ratio_hist[v_ratio_hist_n++] = v_ratio;
+    }
     if (pressure == 0) {
         released_time = time;
-        if (max_v_ratio < 55.0/100.0) {
+        // experimentally found: v_ratio at 2/3 is best
+        double ratio = v_ratio_hist[v_ratio_hist_n*2/3];
+        static FILE * hitlog;
+        if (!hitlog) hitlog = fopen("hit.log", "a");
+        fprintf(hitlog, "%f %d %f %f %f %f\n", ratio, v_ratio_hist_n, duration, v, v_ratio, v_ratio_hist[0]);
+        if (ratio < 45.0/100.0) {
           signal = 1;
           //printf("\7");
           //fflush(stdout);
@@ -58,8 +72,6 @@ int detect_gestures (double dtime, double x, double y, double pressure)
         state = 0;
     } else if (duration > 0.3) {
       state = 99;
-    } else {
-      if (v_ratio > max_v_ratio) max_v_ratio = v_ratio;
     }
   } else {
     assert(state == 99);
@@ -67,9 +79,7 @@ int detect_gestures (double dtime, double x, double y, double pressure)
   }
 
   static FILE * logfile;
-  if (!logfile) {
-    logfile = fopen("data.log", "w");
-  }
+  if (!logfile) logfile = fopen("data.log", "w");
   fprintf(logfile, "%f %f %f %f %f %f %f %f %f\n", time, x, y, pressure, v, vtot.filtered_signal, signal, slow_v.filtered_signal, v_ratio);
 
   if (signal == 1) {
