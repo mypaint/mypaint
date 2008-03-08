b752790a382e576360093530732424b5ea0978f8
Martin Renold <martinxyz@gmx.ch>
2008-03-08 19:45:47 +0000

    prototype using ode solver for brush dynamics
    svn://svn.gna.org/svn/mypaint/trunk@17

--- tilelib/testlib.py
+++ tilelib/testlib.py
@@ -29,9 +29,8 @@ def blendtest():
 
     l.plot()
 
-def painttest():
-    l = tile.TiledLayer()
-    events = load('painting30sec.dat.gz')
+def direct_painttest(l, events):
+    #events = events[:100,:]
     N = len(events)
     dabs = zeros((N, 8), 'float32')
 
@@ -41,9 +40,62 @@ def painttest():
     dabs[:,2] = 3.0 #+ 5.0 * rand(N)
     # rgb
     dabs[:,3:6] = 0.0
-    dabs[:,3] = 1.0
+    dabs[:,4] = 1.0
     # alpha = pressure
     dabs[:,6] = events[:,3]
+    # hardness
+    dabs[:,7] = 0.7
+
+    ctile.render(l, dabs)
+    #l.plot()
+    #l.save('output.png')
+
+
+
+
+def interpolated_painttest():
+    l = tile.TiledLayer()
+    events = load('painting30sec.dat.gz')
+
+    direct_painttest(l, events)
+
+    # states are:
+    # 0 time
+    # 1 filtered x position
+    # 2 filtered y position
+
+    def dstate(state, dab):
+        dstate = ones_like(state)
+        dt = 0.001
+        dstate[0] = dt # time change per dab
+        t = state[0]
+        # find the last event that happened before time t (to be optimized...)
+        i = argmax(events[:,0] > t) - 1
+        if i < 0: i = 0
+        #print dab, t, i, events[i,:], state
+        # change of the filtered x/y position
+        dstate[1:3] = (events[i,1:3] - state[1:3]) * 0.8 # per dab, could also do per time...
+
+        return dstate
+
+
+    N = 3000
+    state0 = array([0.0, events[0,1], events[0,2]])
+    print dstate(state0, 0.0)
+    dabs = arange(N)
+    states = integrate.odeint(dstate, state0, dabs)
+
+    dabs = zeros((N, 8), 'float32')
+
+    # x, y
+    dabs[:,0:2] = states[:,1:3]
+    # radius
+    dabs[:,2] = 3.0 #+ 5.0 * rand(N)
+    # rgb
+    dabs[:,3:6] = 0.0
+    dabs[:,3] = 1.0
+    # alpha = pressure
+    dabs[:,6] = 0.3 # TODO: need pressure state
     # hardness
     dabs[:,7] = 0.7
 
@@ -54,4 +106,5 @@ def painttest():
     #l.plot()
 
 #blendtest()
-painttest()
+#painttest()
+interpolated_painttest()
