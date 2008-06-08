f41fba972bb16865d1647d19d98085916dcf0808
Martin Renold <martinxyz@gmx.ch>
2008-06-08 09:30:26 +0000

    clarify settings
    svn://svn.gna.org/svn/mypaint/branches/0.5@47

--- settingswindow.py
+++ settingswindow.py
@@ -19,13 +19,20 @@ class Window(gtk.Window):
         self.set_title('Settings')
         self.connect('delete-event', self.app.hide_window_cb)
 
+        v_outside = gtk.VBox()
+        self.add(v_outside)
+
+        nb = gtk.Notebook()
+        v_outside.pack_start(nb, expand=True)
+
         v = gtk.VBox()
-        self.add(v)
+        nb.append_page(v, gtk.Label('Pressure'))
 
-        l = gtk.Label('Global Pressure Mapping')
+
+        l = gtk.Label()
         l.set_alignment(0.0, 0.0)
-        v.pack_start(l, expand=False)
-
+        l.set_markup('<b><span size="large">Global Pressure Mapping</span></b>')
+        v.pack_start(l, expand=False, padding=5)
 
         t = gtk.Table(4, 4)
         self.cv = CurveWidget(self.pressure_curve_changed_cb, magnetic=False)
@@ -37,28 +44,30 @@ class Window(gtk.Window):
         t.attach(l2, 3, 4, 1, 2, 0, gtk.EXPAND, 5, 0)
         t.attach(l3, 3, 4, 2, 3, 0, 0, 5, 0)
         l4 = gtk.Label('0.0')
-        l5 = gtk.Label('pressure reported by tablet')
+        l5 = gtk.Label('0.5\n(mouse button pressure)')
+        l5.set_justify(gtk.JUSTIFY_CENTER)
         l6 = gtk.Label('1.0')
         t.attach(l4, 0, 1, 3, 4, 0, 0, 5, 0)
         t.attach(l5, 1, 2, 3, 4, gtk.EXPAND, 0, 5, 0)
         t.attach(l6, 2, 3, 3, 4, 0, 0, 5, 0)
         v.pack_start(t)
 
-        self.ip_cb = gtk.CheckButton('ignore pressure (use click state instead)')
+        v.pack_start(gtk.HSeparator(), expand=False, padding=5)
+
+        self.ip_cb = gtk.CheckButton('ignore pressure and check the mouse button instead')
         self.ip_cb.connect('toggled', self.ignore_pressure_cb)
         v.pack_start(self.ip_cb, expand=False)
 
         h = gtk.HBox()
-
+        h.set_border_width(3)
         b = gtk.Button("Revert")
         b.connect('clicked', self.load_settings)
         h.pack_start(b, expand=False)
-
         b = gtk.Button("Save")
         b.connect('clicked', self.save_settings)
         h.pack_end(b, expand=False)
 
-        v.pack_start(h, expand=False)
+        v_outside.pack_start(h, expand=False)
 
         self.filename = os.path.join(self.app.confpath, 'settings.conf')
 
