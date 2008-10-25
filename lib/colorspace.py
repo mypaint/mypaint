a359f203738fb029fefd4708d6e7f28d87619dd8
Martin Renold <martinxyz@gmx.ch>
2008-10-25 18:14:43 +0000

    fix screwed up "linear light"
    svn://svn.gna.org/svn/mypaint/trunk@110

--- lib/colorspace.py
+++ lib/colorspace.py
@@ -2,7 +2,7 @@
 def float_rgb_lin_to_srgb(src, dst):
     # sRGB gamma correction
     # TODO: not correct, but close enough for a first impression
-    assert src.shape[2] == 3 and dst
+    assert src.shape[2] == 3
     dst[:] = 255*src**(1/2.2)
 
 def srgb_to_float_rgb_lin(src, dst):
