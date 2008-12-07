91dccd5b1ba27aac045b87cbaa76480751e1ce69
Martin Renold <martinxyz@gmx.ch>
2008-12-07 13:09:57 +0000

    removing unmaintained build infrastructure
    svn://svn.gna.org/svn/mypaint/trunk@171

--- autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Run this to generate all the initial makefiles, etc.
-
-echo "autotools build is currently unmaintained, please use SConstruct"
-exit 1
-
-#aclocal -I macros && libtoolize --copy && autoheader && autoconf && automake --add-missing --copy
-aclocal -I macros && libtoolize --copy && autoheader && autoconf && automake --foreign --add-missing --copy
-
