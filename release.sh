a1fe78a15d5965c6614319df8b70aec84bed115a
Martin Renold <martinxyz@gmx.ch>
2009-01-25 20:24:46 +0000

    updating release script
    svn://svn.gna.org/svn/mypaint/trunk@275

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 # this does roughly what 'make distcheck' would do if it did work
 set -e
 
-version=0.5.1
+version=0.6.0
 
 orig=$(pwd)
 d=/tmp/mypaint-$version
@@ -14,18 +14,18 @@ cd $d/html
 rm *.pyc
 cd ..
 rm release.sh
-rpl "SVNVERSION=" "SVNVERSION=$version #" configure.in
-if ! grep "MYPAINT_VERSION='$version'"  drawwindow.py ; then
-    rpl "MYPAINT_VERSION=" "MYPAINT_VERSION='$version' #" drawwindow.py
+#rpl "SVNVERSION=" "SVNVERSION=$version #" configure.in
+if ! grep "MYPAINT_VERSION='$version'"  gui/drawwindow.py ; then
+    echo "Fixing Version in drawwindow.py."
+    rpl "MYPAINT_VERSION=" "MYPAINT_VERSION='$version' #" gui/drawwindow.py
 fi
-./autogen.sh
+#./autogen.sh
 cd ..
 
 filename=$orig/mypaint-$version.tar.bz2
 tar -cvjf $filename mypaint-$version
 
 cd $d
-./configure
-make
+scons
 
 ls -sSh $filename
