e96fe3bbb09c03a316bb052f149c8ea1e64321af
Martin Renold <martinxyz@gmx.ch>
2009-05-24 08:14:04 +0000

    removing some unmaintained scripts
    svn://svn.gna.org/svn/mypaint/trunk@340

--- cross-make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# copied from http://www.libsdl.org/extras/win32/cross/README.txt
-
-PREFIX=/usr/local/cross-tools
-TARGET=i386-mingw32msvc
-PATH="$PREFIX/bin:$PREFIX/$TARGET/bin:$PATH"
-export PATH
-exec make $*
--- distcheck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm -rf /tmp/mypaint-distcheck
-svn export . /tmp/mypaint-distcheck
-cd /tmp/mypaint-distcheck
-scons -j2 && ./mypaint
-
