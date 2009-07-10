a605d125ee01d1dca32aca7608ff8c10cb75d7c0
Martin Renold <martinxyz@gmx.ch>
2009-07-10 12:45:05 +0200

    Run several tests in a row

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import sys, os, tempfile
-from time import time
+import sys, os, tempfile, subprocess
+from time import time, sleep
 
 from pylab import *
 import gtk, glib
@@ -64,7 +64,41 @@ def startup(app):
 
 @gui_test
 def paint(app):
+    """
+    Paint with a constant number of frames per recorded second.
+    Not entirely realistic, but gives good and stable measurements.
+    """
     FPS = 30
+    dw = app.drawWindow
+    tdw = dw.tdw
+
+    for b in app.brushes:
+        if b.name == 'redbrush':
+            app.select_brush(b)
+
+    dw.fullscreen_cb()
+    yield wait_for_idle
+
+    events = load('painting30sec.dat.gz')
+    events = list(events)
+    t0 = time()
+    t_old = 0.0
+    t_last_redraw = 0.0
+    for t, x, y, pressure in events:
+        if t > t_last_redraw + 1.0/FPS:
+            yield wait_for_gui
+            t_last_redraw = t
+        dtime = t - t_old
+        t_old = t
+        dw.doc.stroke_to(dtime, x/tdw.scale, y/tdw.scale, pressure)
+    print 'result =', time()-t0
+
+@gui_test
+def paint_timed(app):
+    """
+    Paint by playing back the recorded events with exact timing. This
+    test takes exactly as long as the recording did.
+    """
     dw = app.drawWindow
     tdw = dw.tdw
 
@@ -85,24 +119,21 @@ def paint(app):
 
 
     events = load('painting30sec.dat.gz')
-    #events[:,0] *= 0.5 # speed
+    #events[:,0] *= 0.5 # speedup
     events = list(events)
     t0 = time()
     t_old = 0.0
     t_last_redraw = 0.0
     for t, x, y, pressure in events:
-        if t > t_last_redraw + 1.0/FPS:
-            yield wait_for_gui
-            t_last_redraw = t
-        #sleeptime = t-(time()-t0)
-        #if sleeptime > 0.001:
-        #    yield sleeptime
+        sleeptime = t-(time()-t0)
+        if sleeptime > 0.001:
+            yield sleeptime
         dtime = t - t_old
         t_old = t
-        #dw.doc.stroke_to(dtime, x, y, pressure)
         dw.doc.stroke_to(dtime, x/tdw.scale, y/tdw.scale, pressure)
-    #print 'replay done.', dw.repaints, 'repaints'
-    print 'replay done, time:', time()-t0
+    print 'replay done,', dw.repaints, 'repaints'
+    # the result is not a time, but let's make it look like one (the smaller the better)
+    print 'result = ', 1.0 / dw.repaints * 1000
 
 @gui_test
 def paint_zoomed_out_5x(app):
@@ -129,10 +160,19 @@ def paint_rotated(app):
         yield res
 
 if __name__ == '__main__':
+    if len(sys.argv) == 3 and sys.argv[1] == 'SINGLE_TEST_RUN':
+        func = all_tests[sys.argv[2]]
+        func()
+        sys.exit(0)
+
     from optparse import OptionParser
-    parser = OptionParser('usage: %prog [options] testname')
+    parser = OptionParser('usage: %prog [options] [test1 test2 test3 ...]')
+    parser.add_option('-a', '--all', action='store_true', default=False, 
+                      help='run all tests')
     parser.add_option('-l', '--list', action='store_true', default=False,
                     help='list all available tests')
+    parser.add_option('-c', '--count', metavar='N', type='int', default=3, 
+                      help='number of repetitions (default: 3)')
     options, tests = parser.parse_args()
 
     if options.list:
@@ -140,14 +180,48 @@ if __name__ == '__main__':
             print name
         sys.exit(0)
 
-    if len(tests) != 1:
-        parser.print_help()
-        sys.exit(1)
-
-    for test in tests:
-        if test not in all_tests:
-            print 'Unknown test:', test
+    if not tests:
+        if options.all:
+            tests = list(all_tests)
+        else:
+            parser.print_help()
             sys.exit(1)
-        func = all_tests[test]
-        func()
 
+    for t in tests:
+        if t not in all_tests:
+            print 'Unknown test:', t
+            sys.exit(1)
+
+    results = []
+    for t in tests:
+        result = []
+        for i in range(options.count):
+            print 'running test "%s" (run %d of %d)' % (t, i+1, options.count)
+            # spawn a new process for each test, to ensure proper cleanup
+            child = subprocess.Popen([sys.argv[0], 'SINGLE_TEST_RUN', t], stdout=subprocess.PIPE)
+            output, trash = child.communicate()
+            if child.returncode != 0:
+                print 'FAILED'
+                break
+            else:
+                print output,
+                try:
+                    value = float(output.split('result = ')[-1].strip())
+                except:
+                    print 'FAILED to find result in test output.'
+                    result = None
+                    break
+                else:
+                    result.append(value)
+        if result is None:
+            sleep(3.0) # some time to press ctrl-c
+        results.append(result)
+    print '=== DETAILS ==='
+    print 'tests =', repr(tests)
+    print 'results =', repr(results)
+    print '=== SUMMARY ==='
+    for t, result in zip(tests, results):
+        if not result:
+            print t, 'FAILED'
+        else:
+            print '%s %.3f' % (t, min(result))
