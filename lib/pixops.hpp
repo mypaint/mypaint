b0549611b3f08582e4baec028ea5edd01cb3d4b8
Martin Renold <martinxyz@gmx.ch>
2009-07-18 15:08:09 +0200

    Portability: use rand() instead of random()
    for Windows build

--- lib/pixops.hpp
+++ lib/pixops.hpp
@@ -146,20 +146,20 @@ void tile_convert_rgba16_to_rgba8(PyObject * src, PyObject * dst) {
       /*
       // Variant B) naive dithering
       // This can alter the alpha channel during a load->save cycle.
-      const uint32_t add_r = random() % (1<<15);
-      const uint32_t add_g = random() % (1<<15);
-      const uint32_t add_b = random() % (1<<15);
-      const uint32_t add_a = random() % (1<<15);
+      const uint32_t add_r = rand() % (1<<15);
+      const uint32_t add_g = rand() % (1<<15);
+      const uint32_t add_b = rand() % (1<<15);
+      const uint32_t add_a = rand() % (1<<15);
       */
 
       // Variant C) slightly better dithering
       // make sure we don't dither rounding errors (those did occur when converting 8bit-->16bit)
       // this preserves the alpha channel, but we still add noise to the highly transparent colors
-      // OPTIMIZE: calling random() slows us down...
-      const uint32_t add_r = (random() % (1<<15)) * 240/256 + (1<<15) * 8/256;
+      // OPTIMIZE: calling rand() slows us down...
+      const uint32_t add_r = (rand() % (1<<15)) * 240/256 + (1<<15) * 8/256;
       const uint32_t add_g = add_r; // hm... do not produce too much color noise
       const uint32_t add_b = add_r;
-      const uint32_t add_a = (random() % (1<<15)) * 240/256 + (1<<15) * 8/256;
+      const uint32_t add_a = (rand() % (1<<15)) * 240/256 + (1<<15) * 8/256;
       // TODO: error diffusion might work better than random dithering...
 
       /*
