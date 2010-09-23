963116e92586f9692d2243111ab7cc0ec7bb61e1
Andrew Chadwick <andrewc-git@piffle.org>
2010-09-23 01:51:55 +0100

    pixbuflist: increase drag threshold
    Subjective, but try 3/4 of the brush icon size feels right to me.
    
    https://gna.org/bugs/?15233

--- gui/pixbuflist.py
+++ gui/pixbuflist.py
@@ -51,6 +51,9 @@ class PixbufList(gtk.DrawingArea):
                         gdk.BUTTON_RELEASE_MASK |
                         gdk.POINTER_MOTION_MASK)
         
+        self.get_settings().set_property("gtk-dnd-drag-threshold",
+            int(min(item_w, item_h) * 0.75))
+
         self.realized_once = False
         self.connect("realize", self.on_realize)
         
