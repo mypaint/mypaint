63b153c6671d658243f1e7d422e6734a68244ad8
David Grundberg <individ@acc.umu.se>
2010-12-12 15:38:26 +0100

    Preferences dialog: Consistent padding and overall layout.
    This changes the layout of the preference dialog to add consistent
    padding.  It also adds headings for each group.  See GNOME HIG.

--- gui/preferenceswindow.py
+++ gui/preferenceswindow.py
@@ -33,56 +33,93 @@ class Window(windowing.Dialog):
 
         # Set up widgets
         nb = gtk.Notebook()
-        self.vbox.pack_start(nb, expand=True, padding=5)
+        nb.set_border_width(12)
+        self.vbox.pack_start(nb, expand=True, padding=0)
 
         ### Input tab
-        v = gtk.VBox()
-        nb.append_page(v, gtk.Label(_('Input')))
+        table = gtk.Table(5, 3)
+        table.set_border_width(12)
+        table.set_col_spacing(0, 12)
+        table.set_col_spacing(1, 12)
+        table.set_row_spacings(6)
+        current_row = 0
+        # TRANSLATORS: Tab label
+        nb.append_page(table, gtk.Label(_('Input')))
+        xopt = gtk.FILL | gtk.EXPAND
+        yopt = gtk.FILL
 
         l = gtk.Label()
-        l.set_alignment(0.0, 0.0)
-        l.set_markup(_('<b><span size="large">Global Pressure Mapping</span></b>'))
-        v.pack_start(l, expand=False, padding=5)
+        l.set_alignment(0.0, 0.5)
+        l.set_markup(_('<b>Input Device</b>'))
+        table.attach(l, 0, 3, current_row, current_row + 1, xopt, yopt)
+        current_row += 1
+
+        l = gtk.Label()
+        l.set_alignment(0.0, 0.5)
+        l.set_line_wrap(True)
+        l.set_markup(_('Scale input pressure to brush pressure. This is applied to all input devices. The mouse button have an input pressure of 0.5 when pressed.'))
+        table.attach(l, 1, 3, current_row, current_row + 1, xopt, yopt)
+        current_row += 1
 
         t = gtk.Table(4, 4)
         self.cv = CurveWidget(self.pressure_curve_changed_cb, magnetic=False)
         t.attach(self.cv, 0, 3, 0, 3, gtk.EXPAND | gtk.FILL, gtk.EXPAND | gtk.FILL, 5, 0)
         l1 = gtk.Label('1.0')
-        l2 = gtk.Label('')
+        if l1.set_angle:
+            # TRANSLATORS: Graph y-axis label
+            l2 = gtk.Label(_('Brush Pressure'))
+            l2.set_angle(90)
+        else:
+            l2 = gtk.Label('')
         l3 = gtk.Label('0.0')
         t.attach(l1, 3, 4, 0, 1, 0, 0, 5, 0)
         t.attach(l2, 3, 4, 1, 2, 0, gtk.EXPAND, 5, 0)
         t.attach(l3, 3, 4, 2, 3, 0, 0, 5, 0)
         l4 = gtk.Label('0.0')
-        l5 = gtk.Label(_('0.5\n(mouse button)'))
+        # TRANSLATORS: Graph x-axis label
+        l5 = gtk.Label(_('Input Pressure'))
         l5.set_justify(gtk.JUSTIFY_CENTER)
         l6 = gtk.Label('1.0')
         t.attach(l4, 0, 1, 3, 4, 0, 0, 5, 0)
         t.attach(l5, 1, 2, 3, 4, gtk.EXPAND, 0, 5, 0)
         t.attach(l6, 2, 3, 3, 4, 0, 0, 5, 0)
-        v.pack_start(t)
+        table.attach(t, 1, 3, current_row, current_row + 1, xopt, yopt)
+        current_row += 1
 
-        v.pack_start(gtk.HSeparator(), expand=False, padding=5)
-
-        h = gtk.HBox()
-        h.pack_start(gtk.Label(_('Mode for input devices: ')), expand=False)
+        l = gtk.Label(_('Mode: '))
+        l.set_alignment(0.0, 0.5)
+        table.attach(l, 1, 2, current_row, current_row + 1, xopt, yopt)
         combo = self.input_devices_combo = gtk.combo_box_new_text()
         for s in device_modes:
             combo.append_text(s)
         combo.connect('changed', self.input_devices_combo_changed_cb)
-        h.pack_start(combo, expand=True)
-        v.pack_start(h, expand=False)
+        table.attach(combo, 2, 3, current_row, current_row + 1, xopt, yopt)
+        current_row += 1
 
-        self.enable_history_popup_checkbox = c = gtk.CheckButton(_('Enable right-click color history'))
+        self.enable_history_popup_checkbox = c = gtk.CheckButton(_('Show color history on right-click'))
         c.connect('toggled', self.enable_history_popup_toggled_cb)
-        v.pack_start(c, expand=False)
+        table.attach(c, 1, 3, current_row, current_row + 1, xopt, yopt)
+        current_row += 1
 
         ### Saving tab
-        saving_vbox = gtk.VBox()
-        nb.append_page(saving_vbox, gtk.Label(_('Saving')))
+        table = gtk.Table(5, 3)
+        table.set_border_width(12)
+        table.set_col_spacing(0, 12)
+        table.set_col_spacing(1, 12)
+        table.set_row_spacings(6)
+        current_row = 0
+        nb.append_page(table, gtk.Label(_('Saving')))
+        xopt = gtk.FILL | gtk.EXPAND
+        yopt = gtk.FILL
 
-        l = gtk.Label(_('Default file format for saving'))
-        l.set_alignment(0.0, 0.0)
+        l = gtk.Label()
+        l.set_alignment(0.0, 0.5)
+        l.set_markup(_('<b>Saving</b>'))
+        table.attach(l, 0, 3, current_row, current_row + 1, xopt, yopt)
+        current_row += 1
+
+        l = gtk.Label(_('Default file format:'))
+        l.set_alignment(0.0, 0.5)
         combo = self.defaultsaveformat_combo = gtk.combo_box_new_text()
         self.defaultsaveformat_values = [filehandling.SAVE_FORMAT_ORA, 
             filehandling.SAVE_FORMAT_PNGSOLID, filehandling.SAVE_FORMAT_JPEG]
@@ -90,29 +127,52 @@ class Window(windowing.Dialog):
             format_desc = self.app.filehandler.saveformats[saveformat][0]
             combo.append_text(format_desc)
         combo.connect('changed', self.defaultsaveformat_combo_changed_cb)
-        saving_vbox.pack_start(l, expand=False)
-        saving_vbox.pack_start(combo, expand=False)
+        table.attach(l, 1, 2, current_row, current_row + 1, xopt, yopt)
+        table.attach(combo, 2, 3, current_row, current_row + 1, xopt, yopt)
+        current_row += 1
 
-        l = gtk.Label(_('Path and filename prefix for "Save Next Scrap"'))
-        l.set_alignment(0.0, 0.0)
-        saving_vbox.pack_start(l, expand=False)
+        l = gtk.Label()
+        l.set_alignment(0.0, 0.5)
+        l.set_markup(_('<b>Save Next Scrap</b>'))
+        table.attach(l, 0, 3, current_row, current_row + 1, xopt, yopt)
+        current_row += 1
+
+        l = gtk.Label(_('Path and filename prefix:'))
+        l.set_alignment(0.0, 0.5)
         self.prefix_entry = gtk.Entry()
         self.prefix_entry.connect('changed', self.prefix_entry_changed_cb)
-        saving_vbox.pack_start(self.prefix_entry, expand=False)
+        table.attach(l, 1, 2, current_row, current_row + 1, xopt, yopt)
+        table.attach(self.prefix_entry, 2, 3, current_row, current_row + 1, xopt, yopt)
+        current_row += 1
 
         ### View tab
-        view_vbox = gtk.VBox()
-        l = gtk.Label(_('Default zoom'))
-        l.set_alignment(0.0, 0.0)
-        nb.append_page(view_vbox, gtk.Label(_('View'))) 
+        table = gtk.Table(2, 3)
+        table.set_border_width(12)
+        table.set_col_spacing(0, 12)
+        table.set_col_spacing(1, 12)
+        table.set_row_spacings(6)
+        current_row = 0
+        nb.append_page(table, gtk.Label(_('View')))
+        xopt = gtk.FILL | gtk.EXPAND
+        yopt = gtk.FILL
+
+        l = gtk.Label()
+        l.set_alignment(0.0, 0.5)
+        l.set_markup(_('<b>Default View</b>'))
+        table.attach(l, 0, 3, current_row, current_row + 1, xopt, yopt)
+        current_row += 1
+
+        l = gtk.Label(_('Default zoom:'))
+        l.set_alignment(0.0, 0.5)
         combo = self.defaultzoom_combo = gtk.combo_box_new_text()
         # Different from doc.zoomlevel_values because we only want a subset
         self.defaultzoom_values = [0.25, 0.50, 1.0, 2.0]
         for val in self.defaultzoom_values:
             combo.append_text('%d%%' % (val*100))
         combo.connect('changed', self.defaultzoom_combo_changed_cb)
-        view_vbox.pack_start(l, expand=False)
-        view_vbox.pack_start(combo, expand=False)
+        table.attach(l, 1, 2, current_row, current_row + 1, xopt, yopt)
+        table.attach(combo, 2, 3, current_row, current_row + 1, xopt, yopt)
+        current_row += 1
 
     def on_response(self, dialog, response, *args):
         if response == gtk.RESPONSE_ACCEPT:
