mypaint/mypaint

commit e1d211436645db1e6be0782b5db0b590d87bd4ef

brushpacks: set persistent flag, ignore ancestry
Set the persistent flag when loading and saving brushes from brushpacks
so they can be highlighted in the gui. Also ignore their ancestry on
save and load to avoid potential wrong-highlight bugs in future.

https://gna.org/bugs/?17157

gui/brushmanager.py
@@ -656,8 +656,11 @@ class ManagedBrush(object):
             self.preview = gdk.Pixbuf(gdk.COLORSPACE_RGB, False, 8, preview_w, preview_h)
             self.preview.fill(0xffffffff) # white
         self.preview.save(prefix + '_prev.png', 'png')
-        open(prefix + '.myb', 'w').write(self.brushinfo.serialize())
+        brushinfo = self.brushinfo.clone()
+        brushinfo.pop("parent_brush_name", None)
+        open(prefix + '.myb', 'w').write(brushinfo.serialize())
         self.remember_mtimes()
+        self.persistent = True
 
     def load(self):
         self.load_preview()
@@ -678,8 +681,10 @@ class ManagedBrush(object):
         filename = prefix + '.myb'
         brushinfo_str = open(filename).read()
         self.brushinfo.parse(brushinfo_str)
+        self.brushinfo.pop("parent_brush_name", None)
         self.remember_mtimes()
         self.settings_loaded = True
+        self.persistent = True
 
     def reload_if_changed(self):
         if self.settings_mtime is None: return

commit e9dfcda7c37d5fcf44ca1c92b17f4495e91160ad

brushmanager: bugfix brushkeys and devbrushes
Commit e1d211436645db1e6be0782b5db0b590d87bd4ef broke brushkeys and
devbrushes' parent relationships. Rollback and fix. In future, we'll
just have to be explicit about whether we want to retain parent names
when loading.

Also, load all brushkey brushes on startup. Not ideal, but the deferred
load doesn't know and shouldn't care if a brush is a brushkey one.

gui/brushmanager.py
@@ -293,12 +293,12 @@ class BrushManager:
             if name.startswith('context'):
                 i = int(name[-2:])
                 self.contexts[i] = b
+                b.load_settings(retain_parent=True)
                 can_be_lost = False
             elif name.startswith(DEVBRUSH_NAME_PREFIX):
                 device_name = devbrush_unquote(name)
                 self.brush_by_device[device_name] = b
-                b.load_settings()
-                b.persistent = False
+                b.load_settings(retain_parent=True)
                 can_be_lost = False
             if can_be_lost:
                 if not [True for group in our.itervalues() if b in group]:
@@ -708,14 +708,14 @@ class ManagedBrush(object):
             self.preview.fill(0xffffffff) # white
         self.preview.save(prefix + '_prev.png', 'png')
         brushinfo = self.brushinfo.clone()
-        brushinfo.pop("parent_brush_name", None)
         open(prefix + '.myb', 'w').write(brushinfo.serialize())
         self.remember_mtimes()
         self.persistent = True
 
-    def load(self):
+    def load(self, retain_parent=False):
+        """Loads the brush's preview and settings from disk."""
         self.load_preview()
-        self.load_settings()
+        self.load_settings(retain_parent)
 
     def load_preview(self):
         """Loads the brush preview as pixbuf into the brush."""
@@ -726,15 +726,16 @@ class ManagedBrush(object):
         self.preview = pixbuf
         self.remember_mtimes()
 
-    def load_settings(self):
+    def load_settings(self, retain_parent=False):
         """Loads the brush settings/dynamics from disk."""
         prefix = self.get_fileprefix()
         filename = prefix + '.myb'
         brushinfo_str = open(filename).read()
         self.brushinfo.parse(brushinfo_str)
-        self.brushinfo.pop("parent_brush_name", None)
         self.remember_mtimes()
         self.settings_loaded = True
+        if not retain_parent:
+            self.brushinfo.pop("parent_brush_name", None)
         self.persistent = True
 
     def reload_if_changed(self):