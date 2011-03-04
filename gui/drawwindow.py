ead66e44b6ee769857c16bc572f6cb9ae6ba9788
Martin Renold <martinxyz@gmx.ch>
2011-03-04 20:43:43 +0100

    version: 0.9.1+git

--- gui/drawwindow.py
+++ gui/drawwindow.py
@@ -13,7 +13,7 @@ This is the main drawing window, containing menu actions.
 Painting is done in tileddrawwidget.py.
 """
 
-MYPAINT_VERSION="0.9.1"
+MYPAINT_VERSION="0.9.1+git"
 
 import os, math, time
 from gettext import gettext as _
