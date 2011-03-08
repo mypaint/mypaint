e13c2af9dbdd340d35363445a8e44a3e39cd5044
Martin Renold <martinxyz@gmx.ch>
2011-03-08 18:26:58 +0100

    brushlib license change (LGPL-->ISC)
    LGPL didn't work anyway, for a headerfile-only library.
    http://en.wikipedia.org/wiki/ISC_license
    
    Clarify that MyPaint is GPL "version 2 or later".

--- brushlib/brushlib.hpp
+++ brushlib/brushlib.hpp
@@ -1,18 +1,5 @@
-/* brushlib - The MyPaint Brush Library
- * Copyright (C) 2007-2008 Martin Renold <martinxyz@gmx.ch>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY. See the COPYING file for more details.
- */
-
 #include "Python.h"
 #include "numpy/arrayobject.h"
 #include "helpers.hpp"
 #include "surface.hpp"
 #include "brush.hpp"
-
--- brushlib/helpers.hpp
+++ brushlib/helpers.hpp
@@ -1,13 +1,17 @@
 /* brushlib - The MyPaint Brush Library
  * Copyright (C) 2007-2008 Martin Renold <martinxyz@gmx.ch>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY. See the COPYING file for more details.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #ifndef __helpers_h__
