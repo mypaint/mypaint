22083a12c1dba693f31f14a96c246db6b523aa10
Jon Nordby <jononor@gmail.com>
2012-05-31 11:53:51 +0200

    brushlib: Add C++ guards to mypaint-gegl-surface.h

--- brushlib/gegl/mypaint-gegl-surface.h
+++ brushlib/gegl/mypaint-gegl-surface.h
@@ -3,6 +3,8 @@
 
 #include <gegl.h>
 
+G_BEGIN_DECLS
+
 typedef struct _MyPaintGeglTiledSurface MyPaintGeglTiledSurface;
 
 GeglBuffer *
@@ -13,5 +15,7 @@ mypaint_gegl_tiled_surface_set_buffer(MyPaintGeglTiledSurface *self, GeglBuffer 
 
 MyPaintGeglTiledSurface *
 mypaint_gegl_tiled_surface_new();
+
+G_END_DECLS
 
 #endif // MYPAINTGEGLSURFACE_H
