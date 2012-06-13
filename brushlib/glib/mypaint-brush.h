32209c4caa815095c9e34792affea0bf61c238c3
Jon Nordby <jononor@gmail.com>
2012-06-13 22:37:46 +0200

    brushlib: Move glib-object.h include inside USE_GLIB conditional

--- brushlib/glib/mypaint-brush.h
+++ brushlib/glib/mypaint-brush.h
@@ -1,12 +1,11 @@
 #ifndef MYPAINTBRUSHGLIB_H
 #define MYPAINTBRUSHGLIB_H
 
-#include <glib-object.h>
-
 #include <mypaint-config.h>
 
 #if MYPAINT_CONFIG_USE_GLIB
 
+#include <glib-object.h>
 #define MYPAINT_TYPE_BRUSH               (mypaint_brush_get_type ())
 #define MYPAINT_VALUE_HOLDS_BRUSH(value) (G_TYPE_CHECK_VALUE_TYPE ((value), MYPAINT_TYPE_BRUSH))
 GType mypaint_brush_get_type(void);
