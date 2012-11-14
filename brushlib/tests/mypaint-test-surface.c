77a9de0284075ae26e6e96b93f923a5b61babd07
Jon Nordby <jononor@gmail.com>
2012-11-14 02:20:37 +0100

    brushlib/tests: Test different brush sizes

--- brushlib/tests/mypaint-test-surface.c
+++ brushlib/tests/mypaint-test-surface.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
 
 #include "mypaint-utils-stroke-player.h"
 #include "mypaint-test-surface.h"
@@ -17,8 +18,10 @@ typedef struct {
     char *test_case_id;
     MyPaintTestsSurfaceFactory factory_function;
     gpointer factory_user_data;
-    int iterations;
+
+    float brush_size;
     float scale;
+    int iterations;
     const char *brush_file;
     SurfaceTransaction surface_transaction;
 } SurfaceTestData;
@@ -28,8 +31,8 @@ test_surface_drawing(void *user_data)
 {
     SurfaceTestData *data = (SurfaceTestData *)user_data;
 
-    const char * event_data = read_file("events/painting30sec.dat");
-    const char * brush_data = read_file(data->brush_file);
+    char * event_data = read_file("events/painting30sec.dat");
+    char * brush_data = read_file(data->brush_file);
 
     assert(event_data);
     assert(brush_data);
@@ -39,6 +42,7 @@ test_surface_drawing(void *user_data)
     MyPaintUtilsStrokePlayer *player = mypaint_utils_stroke_player_new();
 
     mypaint_brush_from_string(brush, brush_data);
+    mypaint_brush_set_base_value(brush, MYPAINT_BRUSH_SETTING_RADIUS_LOGARITHMIC, log(data->brush_size));
 
     mypaint_utils_stroke_player_set_brush(player, brush);
     mypaint_utils_stroke_player_set_surface(player, surface);
@@ -66,7 +70,7 @@ test_surface_drawing(void *user_data)
     char *png_filename = malloc(snprintf(NULL, 0, "%s.png", data->test_case_id) + 1);
     sprintf(png_filename, "%s.png", data->test_case_id);
 
-    mypaint_surface_save_png(surface, png_filename, 0, 0, -1, 1);
+    //mypaint_surface_save_png(surface, png_filename, 0, 0, -1, 1);
     // FIXME: check the correctness of the outputted PNG
 
     free(png_filename);
@@ -74,6 +78,9 @@ test_surface_drawing(void *user_data)
     mypaint_brush_destroy(brush);
     mypaint_surface_destroy(surface);
     mypaint_utils_stroke_player_free(player);
+
+    free(event_data);
+    free(brush_data);
 
     return result;
 }
@@ -95,33 +102,64 @@ mypaint_test_surface_run(int argc, char **argv,
     // distinguish between running test as a benchmark (multiple iterations and taking the time)
     // or as a test (just verifying correctness)
 
-    MyPaintTestsSurfaceFactory factory = surface_factory;
-    gpointer data = user_data;
+    MyPaintTestsSurfaceFactory f = surface_factory;
+    gpointer d = user_data;
 
-    SurfaceTestData test1_data = {create_id("surface=%s,brush=modelling", title),
-                                  factory, data, 1, 4.0,
-                                  "brushes/modelling.myb",
-                                  SurfaceTransactionPerStrokeTo};
-    SurfaceTestData test2_data = {create_id("surface=%s,brush=charcoal", title),
-                                  factory, data, 1, 8.0,
-                                  "brushes/charcoal.myb",
-                                  SurfaceTransactionPerStrokeTo};
-    SurfaceTestData test3_data = {create_id("surface=%s,brush=charcoal,transaction=PerStroke", title),
-                                  factory, data, 1, 8.0,
-                                  "brushes/charcoal.myb",
-                                  SurfaceTransactionPerStroke};
+    SurfaceTestData data[] = {
+        {"1", f, d, 2.0, 1.0, 1, "brushes/modelling.myb", SurfaceTransactionPerStrokeTo},
+        {"2", f, d, 4.0, 1.0, 1, "brushes/modelling.myb", SurfaceTransactionPerStrokeTo},
+        {"3", f, d, 8.0, 1.0, 1, "brushes/modelling.myb", SurfaceTransactionPerStrokeTo},
+        {"4", f, d, 16.0, 2.0, 1, "brushes/modelling.myb", SurfaceTransactionPerStrokeTo},
+        {"5", f, d, 32.0, 2.0, 1, "brushes/modelling.myb", SurfaceTransactionPerStrokeTo},
+        {"6", f, d, 64.0, 2.0, 1, "brushes/modelling.myb", SurfaceTransactionPerStrokeTo},
+        {"7", f, d, 128.0, 4.0, 1, "brushes/modelling.myb", SurfaceTransactionPerStrokeTo},
+        {"8", f, d, 256.0, 4.0, 1, "brushes/modelling.myb", SurfaceTransactionPerStrokeTo},
+        {"9", f, d, 512.0, 4.0, 1, "brushes/modelling.myb", SurfaceTransactionPerStrokeTo},
+        {"10", f, d, 2.0, 1.0, 1, "brushes/charcoal.myb", SurfaceTransactionPerStrokeTo},
+        {"11", f, d, 4.0, 1.0, 1, "brushes/charcoal.myb", SurfaceTransactionPerStrokeTo},
+        {"12", f, d, 8.0, 1.0, 1, "brushes/charcoal.myb", SurfaceTransactionPerStrokeTo},
+        {"13", f, d, 16.0, 2.0, 1, "brushes/charcoal.myb", SurfaceTransactionPerStrokeTo},
+        {"14", f, d, 32.0, 2.0, 1, "brushes/charcoal.myb", SurfaceTransactionPerStrokeTo},
+        {"15", f, d, 64.0, 2.0, 1, "brushes/charcoal.myb", SurfaceTransactionPerStrokeTo},
+        {"16", f, d, 128.0, 4.0, 1, "brushes/charcoal.myb", SurfaceTransactionPerStrokeTo},
+        {"17", f, d, 256.0, 4.0, 1, "brushes/charcoal.myb", SurfaceTransactionPerStrokeTo},
+        {"18", f, d, 512.0, 4.0, 1, "brushes/charcoal.myb", SurfaceTransactionPerStrokeTo},
+        {"19", f, d, 2.0, 1.0, 1, "brushes/coarse_bulk_2.myb", SurfaceTransactionPerStrokeTo},
+        {"20", f, d, 4.0, 1.0, 1, "brushes/coarse_bulk_2.myb", SurfaceTransactionPerStrokeTo},
+        {"21", f, d, 8.0, 1.0, 1, "brushes/coarse_bulk_2.myb", SurfaceTransactionPerStrokeTo},
+        {"22", f, d, 16.0, 2.0, 1, "brushes/coarse_bulk_2.myb", SurfaceTransactionPerStrokeTo},
+        {"23", f, d, 32.0, 2.0, 1, "brushes/coarse_bulk_2.myb", SurfaceTransactionPerStrokeTo},
+        {"24", f, d, 64.0, 2.0, 1, "brushes/coarse_bulk_2.myb", SurfaceTransactionPerStrokeTo},
+        {"25", f, d, 128.0, 2.0, 1, "brushes/coarse_bulk_2.myb", SurfaceTransactionPerStrokeTo},
+        {"26", f, d, 256.0, 2.0, 1, "brushes/coarse_bulk_2.myb", SurfaceTransactionPerStrokeTo},
+//        {"27", f, d, 512.0, 2.0, 1, "brushes/coarse_bulk_2.myb", SurfaceTransactionPerStrokeTo}, // uses to much memory on most machines
+        {"28", f, d, 2.0, 1.0, 1, "brushes/bulk.myb", SurfaceTransactionPerStrokeTo},
+        {"29", f, d, 4.0, 1.0, 1, "brushes/bulk.myb", SurfaceTransactionPerStrokeTo},
+        {"30", f, d, 8.0, 1.0, 1, "brushes/bulk.myb", SurfaceTransactionPerStrokeTo},
+        {"31", f, d, 16.0, 2.0, 1, "brushes/bulk.myb", SurfaceTransactionPerStrokeTo},
+        {"32", f, d, 32.0, 2.0, 1, "brushes/bulk.myb", SurfaceTransactionPerStrokeTo},
+        {"33", f, d, 64.0, 2.0, 1, "brushes/bulk.myb", SurfaceTransactionPerStrokeTo},
+        {"34", f, d, 128.0, 4.0, 1, "brushes/bulk.myb", SurfaceTransactionPerStrokeTo},
+        {"35", f, d, 256.0, 4.0, 1, "brushes/bulk.myb", SurfaceTransactionPerStrokeTo},
+        {"36", f, d, 512.0, 4.0, 1, "brushes/bulk.myb", SurfaceTransactionPerStrokeTo}
+    };
 
-    TestCase test_cases[] = {
-        {test1_data.test_case_id, test_surface_drawing, &test1_data},
-        {test2_data.test_case_id, test_surface_drawing, &test2_data},
-        {test3_data.test_case_id, test_surface_drawing, &test3_data},
+    TestCase test_cases[TEST_CASES_NUMBER(data)];
+    for (int i = 0; i < TEST_CASES_NUMBER(data); i++) {
+        TestCase t;
+        t.id = data[i].test_case_id;
+        t.function = test_surface_drawing;
+        t.user_data = (void *)&data[i];
+        test_cases[i] = t;
     };
 
     int retval = test_cases_run(argc, argv, test_cases, TEST_CASES_NUMBER(test_cases), TEST_CASE_BENCHMARK);
 
-    free(test1_data.test_case_id);
-    free(test2_data.test_case_id);
-    free(test3_data.test_case_id);
+    /*
+    for(int i = 0; i < TEST_CASES_NUMBER(test_cases); i++) {
+        free(test_cases[i].id);
+    }
+    */
 
     return retval;
 }
