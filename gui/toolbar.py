7319d153ad4890413a4ba2440eb8fa44c4a95a05
Andrew Chadwick <andrewc-git@piffle.org>
2012-11-21 02:03:36 +0000

    toolbar: some initial rationalization
    Sort out the viewport manipulation stuff a bit. More options for turning
    irrelevant buttons off. Cluster the icons for special, non-line-drawing
    modes. Subwindow buttons on the right. Replace vertical mirror with
    the more useful fit-to-view action.

--- gui/toolbar.py
+++ gui/toolbar.py
@@ -33,7 +33,9 @@ MERGEABLE_XML = [
     ("toolbar1_edit", 'gui/toolbar-edit.xml', _("Undo and Redo")),
     ("toolbar1_blendmodes", 'gui/toolbar-blendmodes.xml', _("Blend Modes")),
     ("toolbar1_editmodes", 'gui/toolbar-editmodes.xml', _("Editing Modes")),
-    ("toolbar1_view", 'gui/toolbar-view.xml', _("View")),
+    ("toolbar1_view_modes", 'gui/toolbar-view-modes.xml', _("View (Main)")),
+    ("toolbar1_view_manips", 'gui/toolbar-view-manips.xml', _("View (Alternative/Secondary)")),
+    ("toolbar1_view_resets", 'gui/toolbar-view-resets.xml', _("View (Resetting)")),
     ("toolbar1_subwindows", 'gui/toolbar-subwindows.xml', _("Subwindows")),
     ]
 HISTORY_PREVIEW_SIZE = 48
