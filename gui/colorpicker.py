f04c6128359f7a2e61885a209a39a426b0242b0d
Andrew Chadwick <andrewc-git@piffle.org>
2012-12-09 16:57:26 +0000

    colorpicker: better integrate with canvasevent
    Some code love for the color picker:
    
    * Remove hardcoded modifier keys stuff now it can be launched with any
      combination. It still does a "modifiers still held" test, so record
      which modifiers were held when the window appears.
    
    * Both styles of grab fix different bugs, so do both. Other code adopts
      this strategy too.
    
    * Split up the code flow into different event handlers a bit. Slight
      voodoo here, but ensuring the window is shown fully before grabbing
      ay be more correct.
    
    Partly ameliorates https://gna.org/bugs/?20358 by fixing the "first
    launch does not update" problem with the stylus. The rogue grab for
    second and subsequent stylus launches of the picker, which as far as I
    can tell is device specific, is *not* fixed, but can at least now be
    undone by drawing a stroke on the canvas.

--- gui/colorpicker.py
+++ gui/colorpicker.py
@@ -15,20 +15,25 @@ import cairo
 popup_width = 80
 popup_height = 80
 
-class ColorPicker(windowing.PopupWindow):
+
+class ColorPicker (windowing.PopupWindow):
+
     outside_popup_timeout = 0
+
     def __init__(self, app, doc):
         windowing.PopupWindow.__init__(self, app)
+        self.set_can_focus(False)
         # TODO: put the mouse position onto the selected color
 
-        self.add_events(gdk.BUTTON_PRESS_MASK |
-                        gdk.BUTTON_RELEASE_MASK
-                        )
+        self.event_mask = gdk.BUTTON_PRESS_MASK | gdk.BUTTON_RELEASE_MASK
+        self.add_events(self.event_mask)
 
         if pygtkcompat.USE_GTK3:
             self.connect("draw", self.draw_cb)
         else:
             self.connect("expose-event", self.expose_cb)
+        self.connect("show", self.show_cb)
+        self.connect("hide", self.hide_cb)
 
         self.connect("motion-notify-event", self.motion_notify_cb)
 
@@ -37,64 +42,90 @@ class ColorPicker(windowing.PopupWindow):
         self.doc = doc
 
         self.idle_handler = None
-        self.require_ctrl = False
+        self.initial_modifiers = 0
+
 
     def pick(self):
-        # fixed size is prefered to brush radius, see https://gna.org/bugs/?14794
-        #size = int(self.app.brush.get_actual_radius() * math.sqrt(math.pi))
-        #if size < 6: size = 6
+        # fixed size is prefered to brush radius: https://gna.org/bugs/?14794
         size = 6
         self.app.pick_color_at_pointer(self, size)
 
+
     def enter(self):
         self.pick()
-
         # popup placement
         x, y = self.get_position()
         self.move(x, y + popup_height)
+        self.initial_modifiers = 0
         self.show_all()
 
-        # Revert to the older (pre-61cce3b) way of doing a pointer grab, but
-        # set the cursor here for the duration of the grab. I think this is a
-        # smarter way of doing it...
-        result = gdk.pointer_grab(self.window,
-                                  event_mask=gdk.POINTER_MOTION_MASK,
-                                  cursor=self.app.cursor_color_picker)
+
+    def show_cb(self, widget):
+
+        # Grab the pointer
+        # This is required for the color picker to pick continuously, and for
+        # the color picker window to disappear after being launched with a
+        # mouse button binding. https://gna.org/bugs/?19710
+        result = gdk.pointer_grab(self.get_window(), False,
+                event_mask=self.event_mask | gdk.POINTER_MOTION_MASK,
+                cursor=self.app.cursor_color_picker)
         if result != gdk.GRAB_SUCCESS:
             print 'Warning: pointer grab failed with result', result
             self.leave(reason=None)
             return
 
+        # A GTK-level grab helps for some reason. Without it, the first time
+        # the window is shown, it doesn't receive motion-notify events as
+        # described in https://gna.org/bugs/?20358
+        self.grab_add()
+
         self.popup_state.register_mouse_grab(self)
 
-        self.require_ctrl = False
+        # Record the modifiers which were held when the window was shown.
+        display = gdk.display_get_default()
+        screen, x, y, modifiers = display.get_pointer()
+        modifiers &= gtk.accelerator_get_default_mod_mask()
+        self.initial_modifiers = modifiers
+
 
     def leave(self, reason):
+        self.hide()
+
+
+    def hide_cb(self, window):
+        # Undo open grabs.
+        self.grab_remove()
         gdk.pointer_ungrab()
 
+        # Disconnect update idler
         if self.idle_handler:
             gobject.source_remove(self.idle_handler)
             self.idle_handler = None
-        self.hide()
+
 
     def motion_notify_cb(self, widget, event):
-        if event.state & gdk.CONTROL_MASK or event.state & gdk.MOD1_MASK:
-            self.require_ctrl = True
-        elif self.require_ctrl:
-            # stop picking when the user releases CTRL (or ALT)
-            return
-
-        def update():
-            self.idle_handler = None
-            self.pick()
-            self.queue_draw()
-
+        if self.initial_modifiers:
+            modifiers = event.state & gtk.accelerator_get_default_mod_mask()
+            if modifiers & self.initial_modifiers == 0:
+                # stop picking when the user releases CTRL (or ALT)
+                self.leave("initial modifiers no longer held")
+                self.initial_modifiers = 0
+                return
         if not self.idle_handler:
-            self.idle_handler = gobject.idle_add(update)
+            self.idle_handler = gobject.idle_add(self.motion_update_idler)
+
+
+    def motion_update_idler(self):
+        self.pick()
+        self.queue_draw()
+        self.idle_handler = None
+        return False
+
 
     def expose_cb(self, widget, event):
         cr = self.window.cairo_create()
         return self.draw_cb(widget, cr)
+
 
     def draw_cb(self, widget, cr):
         #cr.set_source_rgb (1.0, 1.0, 1.0)
@@ -119,5 +150,5 @@ class ColorPicker(windowing.PopupWindow):
         cr.set_line_width(line_width)
         cr.set_source_rgb(0, 0, 0)
         cr.stroke()
-        
+
         return True
