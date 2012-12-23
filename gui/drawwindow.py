5ed8bcd3f0adc1f5b48a1fa95e521018de20bb97
Martin Renold <martinxyz@gmx.ch>
2012-12-23 17:59:16 +0100

    version bump

--- gui/drawwindow.py
+++ gui/drawwindow.py
@@ -13,7 +13,7 @@ This is the main drawing window, containing menu actions.
 Painting is done in tileddrawwidget.py.
 """
 
-MYPAINT_VERSION="1.0.0+git"
+MYPAINT_VERSION="1.1.0"
 
 import os, time, webbrowser
 from warnings import warn
