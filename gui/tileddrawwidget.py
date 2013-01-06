e449661a11ed4320b7164268567ec670b700990f
Martin Renold <martinxyz@gmx.ch>
2013-01-06 19:03:51 +0100

    when zoomed-in, pixelize a bit earlier

--- gui/tileddrawwidget.py
+++ gui/tileddrawwidget.py
@@ -759,7 +759,7 @@ class CanvasRenderer(gtk.DrawingArea, DrawCursorMixin):
             #pattern.set_filter(cairo.FILTER_BEST)     # 3.1s
             #pattern.set_filter(cairo.FILTER_BILINEAR) # 3.1s
 
-            if self.scale > 3.0:
+            if self.scale > 2.8:
                 # pixelize at high zoom-in levels
                 pattern.set_filter(cairo.FILTER_NEAREST)
 
