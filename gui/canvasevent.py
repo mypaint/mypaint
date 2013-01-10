d8d80532fce9dceab26b901876131947eb7af5dd
Andrew Chadwick <andrewc-git@piffle.org>
2013-01-10 01:39:35 +0000

    gtk3: fix picker getting stuck in grab
    GI+GTK3 reports button numbers strangely when peeking at the last event
    on the stack (something horrid we need to do when responding to a
    non-keyboard-iniaiated Action, e.g. a mouse menu selection or a toolbar
    button press). Record a null button press initially, and let the first
    subsequent button-press record the number so that the release works.
    
    This code is getting somewhat hairy. Do we need the preview thing at
    all? It's cute, but I count two other colour previews on my screen most
    of the time. It's also somewhat slow for GTK3 mode, perhaps because the
    screen pixel reading code itself is slower? Definitely breaks the
    feeling of continuity in a hold-and-drag pick.
    
    For fullscreen mode, when it matters, could it be converted to an
    optional fading overlay that just triggers and then fades out whenever
    the colour changes? Perhaps near the pointer, perhaps in a corner.
    Something similar for brushes might be nice too.

--- gui/canvasevent.py
+++ gui/canvasevent.py
@@ -1145,17 +1145,20 @@ class DragMode (InteractionMode):
 
     def _force_drag_start(self):
         # Attempt to force a drag to start, using the current event.
+
+        # XXX: This is only used by the picker mode, which needs to begin
+        # picking straight away in enter even if this is in response to an
+        # action activation.
         event = gtk.get_current_event()
-        self._fake_drag_start(event)
-
-
-    def _fake_drag_start(self, event):
         if event is None:
             print "no event"
             return
         if self.in_drag:
             return
         tdw = self.doc.tdw
+        # Duck-profile the starting event. If it's a keypress event or a
+        # button-press event, or anything that quacks like those, we can
+        # attempt the grab and start the drag if it succeeded.
         if hasattr(event, "keyval"):
             if event.keyval != self._start_keyval:
                 self._start_keyval = event.keyval
@@ -1167,7 +1170,16 @@ class DragMode (InteractionMode):
                 # Grab succeeded
                 self.last_x = event.x
                 self.last_y = event.y
-                self._start_button = event.button
+                # For the toolbar button, and for menus, it's a button-release
+                # event.
+                # Record which button is being pressed at start
+                if type(event.button) == type(0):
+                    # PyGTK supplies the actual button number ...
+                    self._start_button = event.button
+                else:
+                    # ... but GI+GTK3 supplies a <void at 0xNNNNNNNN> object
+                    # when the event comes from gtk.get_current_event() :(
+                    self._start_button = None
 
 
 class SpringLoadedDragMode (SpringLoadedModeMixin, DragMode):
