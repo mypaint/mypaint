cf806b70c2bd1a4bac25cc75f467977594e1bcb0
Andrew Chadwick <andrewc-git@piffle.org>
2013-03-20 22:55:40 +0000

    layerswindow: add standard copyright notice
    Based on its first commit. Yes, we should update these, and be more careful
    about including them too.

--- gui/layerswindow.py
+++ gui/layerswindow.py
@@ -1,3 +1,11 @@
+# This file is part of MyPaint.
+# Copyright (C) 2009 by Ilya Portnov <portnov@bk.ru>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
 import pygtkcompat
 
 import gtk
