3e320aaec916d367f7b67d92429766e3c1c7da34
Andrew Chadwick <andrewc-git@piffle.org>
2013-03-21 15:06:35 +0000

    glade: update for glade3
    * Generic names are now alloneword.
    * Remove mypaint_widgets.py and support for gladepython. Not there in glade3.
    * Now faking widgets with a gtkspinner and some rough sizes.
    * Remove hsvcompat: no longer present in the MyPaint source.
    * Launcher script update.
    * Update docs.
    * Initial support for ButtonMappingEditor.

--- glade/run.sh
+++ glade/run.sh
@@ -2,18 +2,9 @@
 # Runs the Glade UI designer with a local catalog, for design and development
 # purposes. No need to install the catalog files.
 
-TOPDIR="`dirname $0`/.."
-GLADE_DEFS_FILE="mypaint.glade"
-GLADE_CATALOG_PATH="glade"
-XDG_DATA_DIRS="$GLADE_CATALOG_PATH:$XDG_DATA_DIRS"
-
-cd "$TOPDIR"
-export GLADE_CATALOG_PATH
+GLADE_CATALOG_SEARCH_PATH="`dirname $0`"
+XDG_DATA_DIRS="$GLADE_CATALOG_SEARCH_PATH:$XDG_DATA_DIRS"
+export GLADE_CATALOG_SEARCH_PATH
 export XDG_DATA_DIRS
-if test -f $GLADE_DEFS_FILE; then
-    exec glade $GLADE_DEFS_FILE
-else
-    echo "Create $GLADE_DEFS_FILE and I'll load it for you next time."
-    exec glade
-fi
+exec glade "$@"
 
