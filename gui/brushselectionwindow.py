9875595cf4bcd2b3d6d065fa4d3d5049fe3d00d4
Andrew Chadwick <andrewc-git@piffle.org>
2013-03-24 01:06:26 +0000

    brushselectionwindow: minor code cleanup
    Minor code tidying; no functional changes. Whitespace, layout, and 0-arg
    construction for classes we might one day hope to make Buildable.

--- gui/brushselectionwindow.py
+++ gui/brushselectionwindow.py
@@ -13,6 +13,8 @@ Responsible for ordering, loading and saving brush lists.
 
 """
 
+import platform
+
 import pygtkcompat
 
 from gettext import gettext as _
@@ -23,6 +25,7 @@ import pango
 import gtk
 from gtk import gdk
 
+from application import get_app
 import pixbuflist
 import dialogs
 import brushmanager
@@ -43,8 +46,8 @@ class ToolWidget (gtk.VBox):
 
         self.last_selected_brush = None
 
-        self.groupselector = GroupSelector(self.app)
-        self.brushgroups = BrushGroupsList(self.app)
+        self.groupselector = GroupSelector()
+        self.brushgroups = BrushGroupsList()
 
         self.scroll = scroll = gtk.ScrolledWindow()
         scroll.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
@@ -185,9 +188,15 @@ class BrushList(pixbuflist.PixbufList):
 
         self.bm.select_brush(brush)
 
+
+
 class BrushGroupsList(gtk.VBox):
-    def __init__(self, app):
+    """A vertical stack of brush lists.
+    """
+
+    def __init__(self):
         gtk.VBox.__init__(self)
+        app = get_app()
         self.app = app
         self.bm = app.brushmanager
         self.group_widgets = {}
@@ -217,13 +226,25 @@ class BrushGroupsList(gtk.VBox):
 
         self.show_all()
 
-class GroupSelector(gtk.DrawingArea):
+
+
+class GroupSelector (gtk.DrawingArea):
+    """Brush group selector widget.
+
+    Shows a line-wrapped list of group names, any or all of which may be
+    selected or unselected with the pointer device. Toggling groups on or off
+    this way updates the associated BrushGroupsList to show different groups.
+
+    """
+
 
     VERTICAL_MARGIN = 2
 
-    def __init__(self, app):
+
+    def __init__(self):
         gtk.DrawingArea.__init__(self)
 
+        app = get_app()
         self.app = app
         self.bm = app.brushmanager
         self.bm.groups_observers.append(self.active_groups_changed_cb)
@@ -268,13 +289,13 @@ class GroupSelector(gtk.DrawingArea):
     def active_groups_changed_cb(self):
         self.queue_draw()
 
+
     def lay_out_group_names(self, width):
         "Typeset the group names into a new Pango layout at a given width"
         self.ensure_style()
         style = self.get_style()
         layout = pango.Layout(self.get_pango_context())
         layout.set_width(width*pango.SCALE)
-        #layout.set_font_description(style.font_desc) # Needed?
 
         all_groups = list(sorted(self.bm.groups.keys()))
         idx = 0
@@ -286,7 +307,6 @@ class GroupSelector(gtk.DrawingArea):
         pad_s = u"\u202f"  # NARROW NO-BREAK SPACE
         sp_s = pad_s + u"\u200b"  # ZERO WIDTH SPACE
 
-        import platform
         if platform.system() == 'Windows' or platform.system() == 'Darwin':
             # workaround for https://gna.org/bugs/?15192
             pad_s = ''
@@ -336,6 +356,7 @@ class GroupSelector(gtk.DrawingArea):
         layout.set_spacing(leading)
         return layout
 
+
     def on_size_request(self, widget, req):
         # PyGTK/gtk2 size-request handler
         parent = self.parent.parent
@@ -348,6 +369,7 @@ class GroupSelector(gtk.DrawingArea):
         h += 2 * self.VERTICAL_MARGIN
         req.width = -1
         req.height = h
+
 
     def do_get_request_mode(self):
         # PyGI/gtk3 sizing
@@ -371,6 +393,7 @@ class GroupSelector(gtk.DrawingArea):
     def expose_cb(self, widget, event):
         cr = self.get_window().cairo_create()
         return self.draw_cb(widget, cr)
+
 
     def draw_cb(self, widget, cr):
         alloc = self.get_allocation()
@@ -406,6 +429,7 @@ class GroupSelector(gtk.DrawingArea):
 
         self.layout = layout
 
+
     def group_at(self, x, y):
         x, y = int(x), int(y) # avoid warning
         if self.layout is None:
@@ -417,6 +441,7 @@ class GroupSelector(gtk.DrawingArea):
             i, trailing = index_tup
         return self.idx2group.get(i)
 
+
     def button_press_cb(self, widget, event):
         if event.type != gdk.BUTTON_PRESS:
             return # double or tripple click
@@ -425,10 +450,11 @@ class GroupSelector(gtk.DrawingArea):
         if event.button in [1, 2]:
             if not group:
                 return
-
             active_groups = self.bm.active_groups[:]
-
-            if event.state & gdk.CONTROL_MASK or event.state & gdk.SHIFT_MASK or event.button == 2:
+            is_toggle_request = (event.state & gdk.CONTROL_MASK or
+                                 event.state & gdk.SHIFT_MASK or
+                                 event.button == 2)
+            if is_toggle_request:
                 # toggle group visibility
                 if group in active_groups:
                     active_groups.remove(group)
@@ -437,7 +463,6 @@ class GroupSelector(gtk.DrawingArea):
             else:
                 # group solo
                 active_groups = [group]
-
             self.bm.set_active_groups(active_groups)
 
         elif event.button == 3:
@@ -454,11 +479,13 @@ class GroupSelector(gtk.DrawingArea):
                        func=_pos, button=event.button,
                        activate_time=event.time, data=data)
 
+
     def motion_notify_cb(self, widget, event):
         old_prelight_group = self.gtkstate_prelight_group
         self.gtkstate_prelight_group = self.group_at(event.x, event.y)
         if self.gtkstate_prelight_group != old_prelight_group:
             self.queue_draw()
+
 
     def leave_notify_cb(self, widget, event):
         old_prelight_group = self.gtkstate_prelight_group
@@ -466,6 +493,7 @@ class GroupSelector(gtk.DrawingArea):
         if self.gtkstate_prelight_group != old_prelight_group:
             self.gtkstate_prelight_group = None
             self.queue_draw()
+
 
     def context_menu(self, group):
         m = gtk.Menu()
@@ -483,7 +511,6 @@ class GroupSelector(gtk.DrawingArea):
         for entry in menu:
             if entry is None:
                 item = gtk.SeparatorMenuItem()
-
             else:
                 label, callback = entry
                 item = gtk.MenuItem(label)
@@ -493,14 +520,17 @@ class GroupSelector(gtk.DrawingArea):
         m.show_all()
         return m
 
+
     def menu_finished_cb(self, menushell):
         self.gtkstate_active_group = None
         self.queue_draw()
+
 
     def create_group_cb(self, w, group):
         new_group = dialogs.ask_for_name(self, _('Create Group'), '')
         if new_group:
             self.bm.create_group(new_group)
+
 
     def rename_group_cb(self, w, old_group):
         new_group = dialogs.ask_for_name(self, _('Rename Group'), old_group)
@@ -511,6 +541,7 @@ class GroupSelector(gtk.DrawingArea):
         else:
             dialogs.error(self, _('A group with this name already exists!'))
 
+
     def export_group_cb(self, w, group):
         format_id, filename = dialogs.save_dialog(_("Export Brushes"), None,
                                  [(_("MyPaint brush package (*.zip)"), "*.zip")],
@@ -518,13 +549,20 @@ class GroupSelector(gtk.DrawingArea):
         if filename is not None:
             self.bm.export_group(group, filename)
 
+
     def delete_group_cb(self, w, group):
-        if dialogs.confirm(self, _('Really delete group %s?') % brushmanager.translate_group_name(group)):
+        msg = _('Really delete group %s?') \
+                % brushmanager.translate_group_name(group)
+        if dialogs.confirm(self, msg):
             self.bm.delete_group(group)
             if group in self.bm.groups:
-                dialogs.error(self, _('This group can not be deleted (try to empty it first).'))
+                msg = _('This group can not be deleted '
+                        '(try to empty it first).')
+                dialogs.error(self, msg)
 
-    def drag_data_received_cb(self, widget, context, x, y, selection, targetType, time):
+
+    def drag_data_received_cb(self, widget, context, x, y, selection,
+                              targetType, time):
         """
         Respond to some data being dropped via dnd onto the list of groups.
         This typically results in a brush being copied or moved into the group
@@ -552,6 +590,7 @@ class GroupSelector(gtk.DrawingArea):
             context.finish(False, False, time)
         for f in self.bm.brushes_observers: f(changed)
         context.finish(True, False, time)
+
 
     def drag_motion_cb(self, widget, context, x, y, time):
         """
@@ -589,6 +628,7 @@ class GroupSelector(gtk.DrawingArea):
         if group != self.drag_target_group:
             self.drag_target_group = group
             self.queue_draw()
+
 
     def drag_clear_cb(self, widget, context, time):
         """
