9d5513f32d46a984a16e96fa4bf7d96002ce0dcf
Andrew Chadwick <andrewc-git@piffle.org>
2013-03-27 03:14:00 +0000

    glade: fix runner script
    Certain installations of Glade have no hardcoded data search path, and bomb
    if they can't find icons for their main shipped catalogs. Assume the spec
    defaults for $XDG_DATA_DIRS if it's not set prior to overriding it.

--- glade/run.sh
+++ glade/run.sh
@@ -2,8 +2,13 @@
 # Runs the Glade UI designer with a local catalog, for design and development
 # purposes. No need to install the catalog files.
 
-GLADE_CATALOG_SEARCH_PATH="`dirname $0`"
-XDG_DATA_DIRS="$GLADE_CATALOG_SEARCH_PATH:$XDG_DATA_DIRS"
+dir="`dirname $0`"
+GLADE_CATALOG_SEARCH_PATH="$dir"
+if test "x$XDG_DATA_DIRS" = "x"; then
+    XDG_DATA_DIRS="/usr/local/share/:/usr/share/"
+fi
+XDG_DATA_DIRS="${dir}:${XDG_DATA_DIRS}"
+
 export GLADE_CATALOG_SEARCH_PATH
 export XDG_DATA_DIRS
 exec glade "$@"
