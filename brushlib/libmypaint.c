af9cae36c90f3c82a69d717f28a718e7aa893b53
Jon Nordby <jononor@gmail.com>
2013-04-09 18:25:01 +0200

    brushlib: Add a "libmypaint.c" that can be included directly
    Means people don't need to build libmypaint separately.

---
+++ brushlib/libmypaint.c
@@ -0,0 +1,18 @@
+
+
+
+#include "mapping.c"
+#include "helpers.c"
+#include "brushmodes.c"
+#include "fifo.c"
+#include "operationqueue.c"
+#include "rng-double.c"
+#include "utils.c"
+
+#include "mypaint.c"
+#include "mypaint-brush.c"
+#include "mypaint-brush-settings.c"
+#include "mypaint-fixed-tiled-surface.c"
+#include "mypaint-surface.c"
+#include "mypaint-tiled-surface.c"
+#include "mypaint-rectangle.c"
