58ec47436f03f02cc5c1f6c5fb2153fc219dcc13
Jon Nordby <jononor@gmail.com>
2013-04-09 20:17:56 +0200

    brushlib: Also commit generated mypaint-config.h
    This one basically never changes, as it only contains the
    definition of MYPAINT_TILE_SIZE which has been 64 for years.

---
+++ brushlib/mypaint-config.h
@@ -0,0 +1,13 @@
+#ifndef MYPAINTCONFIG_H
+#define MYPAINTCONFIG_H
+
+#ifndef MYPAINT_TILE_SIZE
+#define MYPAINT_TILE_SIZE 64
+#endif
+
+// Start generated config
+#define MYPAINT_CONFIG_USE_GLIB 0
+
+// End generated config
+
+#endif // MYPAINTCONFIG_H
