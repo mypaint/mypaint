0d40a260f1465fbe4d4bb890193959b7a418a43e
Jon Nordby <jononor@gmail.com>
2013-05-01 12:55:03 +0200

    brushlib: Fix minimal.c example
    Need to call begin/end_atomic() around modifications, and to load
    sensible default values for the brush. Also tweak the dtime to
    a more sensible value.

--- brushlib/examples/minimal.c
+++ brushlib/examples/minimal.c
@@ -7,8 +7,8 @@
 void
 stroke_to(MyPaintBrush *brush, MyPaintSurface *surf, float x, float y)
 {
-    float pressure = 1.0, ytilt = 0.0, xtilt = 0.0, dtime = 100.0;
-    mypaint_brush_stroke_to(brush, surf, 0.0, 0.0, pressure, xtilt, ytilt, dtime);
+    float pressure = 1.0, ytilt = 0.0, xtilt = 0.0, dtime = 1.0/10;
+    mypaint_brush_stroke_to(brush, surf, x, y, pressure, xtilt, ytilt, dtime);
 }
 
 int
@@ -17,12 +17,19 @@ main(int argc, char argv[]) {
     MyPaintBrush *brush = mypaint_brush_new();
     MyPaintFixedTiledSurface *surface = mypaint_fixed_tiled_surface_new(500, 500);
 
+    mypaint_brush_from_defaults(brush);
+    mypaint_brush_set_base_value(brush, MYPAINT_BRUSH_SETTING_COLOR_H, 0.0);
+    mypaint_brush_set_base_value(brush, MYPAINT_BRUSH_SETTING_COLOR_S, 1.0);
+    mypaint_brush_set_base_value(brush, MYPAINT_BRUSH_SETTING_COLOR_V, 1.0);
+
     /* Draw a rectangle on surface with brush */
+    mypaint_surface_begin_atomic((MyPaintSurface *)surface);
     stroke_to(brush, (MyPaintSurface *)surface, 0.0, 0.0);
     stroke_to(brush, (MyPaintSurface *)surface, 200.0, 0.0);
     stroke_to(brush, (MyPaintSurface *)surface, 200.0, 200.0);
     stroke_to(brush, (MyPaintSurface *)surface, 0.0, 200.0);
     stroke_to(brush, (MyPaintSurface *)surface, 0.0, 0.0);
+    mypaint_surface_end_atomic((MyPaintSurface *)surface);
 
     fprintf(stdout, "Writing output\n");
     write_ppm(surface, "output.ppm");
