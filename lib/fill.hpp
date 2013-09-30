febbe2ff60e2662c3eec94b6decbb0e02c6b8ed2
Andrew Chadwick <andrewc-git@piffle.org>
2013-09-30 19:15:35 +0100

    fill: use the GIMP's matching algorithm
    Use the GIMP's colour matching algorithm during fill: it gives better
    results and allows fills to butt up against anti-aliased lines more
    convincingly with larger thresholds.

--- lib/fill.hpp
+++ lib/fill.hpp
@@ -32,17 +32,20 @@ _floodfill_getpixel(PyArrayObject *array,
                             + (y * ystride)
                             + (x * xstride));
 }
-                    
 
-/* _floodfill_color_dist:
 
-Distance metric used for flood fill. Euclidean distance between two
-premultiplied RGBA colours. Result is a scaled int in the range [0.0, 1.0].
+/* _floodfill_color_match:
+
+Similarity metric used by flood fill.  Result is a fix15_t in the range [0.0,
+1.0], with zero meaning no match close enough.  Similar algorithm to the GIMP's
+pixel_difference(): a threshold is used for a "similar enough" determination.
+
 */
 
 static inline fix15_t
-_floodfill_color_dist(const fix15_short_t c1_premult[4],
-                      const fix15_short_t c2_premult[4])
+_floodfill_color_match(const fix15_short_t c1_premult[4],
+                       const fix15_short_t c2_premult[4],
+                       const fix15_t tolerance)
 {
     const fix15_short_t c1_a = c1_premult[3];
     fix15_short_t c1[] = {
@@ -58,13 +61,48 @@ _floodfill_color_dist(const fix15_short_t c1_premult[4],
         fix15_short_clamp(c2_a <= 0 ? 0 : fix15_div(c2_premult[2], c2_a)),
         fix15_short_clamp(c2_a),
     };
-    fix15_t sumsqdiffs = 0;
+
+    // Calculate the raw distance
+    fix15_t dist = 0;
     for (int i=0; i<4; ++i) {
         fix15_t n = (c1[i] > c2[i]) ? (c1[i] - c2[i]) : (c2[i] - c1[i]);
-        n >>= 2; // quarter, to avoid a fixed maths sqrt() overflow
-        sumsqdiffs += fix15_mul(n, n);
+        if (n > dist)
+            dist = n;
     }
-    return fix15_sqrt(sumsqdiffs) << 1;  // [0.0 .. 0.5], doubled
+    /*
+     * // Alternatively, could use
+     * fix15_t sumsqdiffs = 0;
+     * for (int i=0; i<4; ++i) {
+     *     fix15_t n = (c1[i] > c2[i]) ? (c1[i] - c2[i]) : (c2[i] - c1[i]);
+     *     n >>= 2; // quarter, to avoid a fixed maths sqrt() overflow
+     *     sumsqdiffs += fix15_mul(n, n);
+     * }
+     * dist = fix15_sqrt(sumsqdiffs) << 1;  // [0.0 .. 0.5], doubled
+     * // but the max()-based metric will a) be more GIMP-like and b) not
+     * // lose those two bits of precision.
+     */
+
+    // Compare with adjustable tolerance of mismatches.
+    static const fix15_t onepointfive = fix15_one + fix15_halve(fix15_one);
+    if (tolerance > 0) {
+        dist = fix15_div(dist, tolerance);
+        if (dist > onepointfive) {  // aa < 0, but avoid underflow
+            return 0;
+        }
+        else {
+            fix15_t aa = onepointfive - dist;
+            if (aa < fix15_halve(fix15_one))
+                return fix15_short_clamp(fix15_double(aa));
+            else
+                return fix15_one;
+        }
+    }
+    else {
+        if (dist > tolerance)
+            return 0;
+        else
+            return fix15_one;
+    }
 }
 
 
@@ -83,7 +121,7 @@ _floodfill_should_fill(const fix15_short_t src_col[4], // premult RGB+A
     if (dst_col[3] != 0) {
         return false;   // already filled
     }
-    return _floodfill_color_dist(src_col, targ_col) <= tolerance;
+    return _floodfill_color_match(src_col, targ_col, tolerance) > 0;
 }
 
 
@@ -212,11 +250,12 @@ tile_flood_fill (PyObject *src, /* readonly HxWx4 array of uint16 */
                 // Fill this pixel, and continue iterating in this direction
                 fix15_t alpha = fix15_one;
                 if (tolerance > 0) {
-                    fix15_t dist = _floodfill_color_dist(targ, src_pixel);
-                    dist = fix15_short_clamp(fix15_div(dist, tolerance));
-                    alpha = fix15_short_clamp(fix15_one - dist);
+                    alpha = _floodfill_color_match(targ, src_pixel,
+                                                   tolerance);
+                    // Since we use the output array to mark where we've been
+                    // during the fill, we can't store an alpha of zero.
                     if (alpha == 0) {
-                        alpha = 0x0001; // can't have zero
+                        alpha = 0x0001;
                     }
                 }
                 dst_pixel[0] = fix15_short_clamp(fill_r * alpha);
