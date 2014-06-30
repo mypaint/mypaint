mypaint/mypaint

commit 6414ab0b77da11850d5ef4d616b5d464af29d8f4

tiledsurface: optimize dst-in compositing
Don't combine tiles in dst-in mode if we don't have to.  Clearing seems
faster.  We now make the assumption that this is the only mode that erases
its backdrop at zero alpha.

Partly addresses #21.

lib/tiledsurface.py
@@ -82,12 +82,6 @@ class MyPaintSurface (object):
     The C++ part of this class is in tiledsurface.hpp
     """
 
-    #: Lookup table of combine modes to skip whan alpha==0, keyed by mode
-    _SKIP_COMPOSITE_IF_EMPTY = [
-        not mypaintlib.combine_mode_get_info(mode)["zero_alpha_has_effect"]
-        for mode in xrange(mypaintlib.NumCombineModes) ]
-
-
     def __init__(self, mipmap_level=0, mipmap_surfaces=None,
                  looped=False, looped_size=(0,0)):
         object.__init__(self)
@@ -288,21 +282,39 @@ class MyPaintSurface (object):
                        opacity=1.0, mode=mypaintlib.CombineNormal):
         """Composite one tile of this surface over a NumPy array.
 
-        Composite one tile of this surface over the array dst, modifying only dst.
-        """
-        if self.mipmap_level < mipmap_level:
-            return self.mipmap.composite_tile(dst, dst_has_alpha, tx, ty,
-                                              mipmap_level, opacity, mode)
+        :param dst: target tile array (uint16, NxNx4, 15-bit scaled int)
+        :param dst_has_alpha: alpha channel in dst should be preserved
+        :param tx: tile X coordinate, in model tile space
+        :param ty: tile Y coordinate, in model tile space
+        :param mipmap_level: layer mipmap level to use
+        :param opacity: opacity multiplier
+        :param mode: mode to use when compositing
 
-        # Optimization: for some compositing modes, e.g. source-over, an empty
-        # source tile leaves the backdrop unchanged.
-        if self._SKIP_COMPOSITE_IF_EMPTY[mode]:
-            if (tx, ty) not in self.tiledict:
+        Composite one tile of this surface over the array dst,
+        modifying only dst.
+        """
+
+        if opacity == 0:
+            if mode == mypaintlib.CombineDestinationIn:
+                if dst_has_alpha:
+                    mypaintlib.tile_clear(dst)
+                    return
+            else:
                 return
-            if opacity == 0:
-                return
+
+        if self.mipmap_level < mipmap_level:
+            self.mipmap.composite_tile(dst, dst_has_alpha, tx, ty,
+                                       mipmap_level, opacity, mode)
+            return
 
         with self.tile_request(tx, ty, readonly=True) as src:
+            if src is transparent_tile.rgba:
+                if mode == mypaintlib.CombineDestinationIn:
+                    if dst_has_alpha:
+                        mypaintlib.tile_clear(dst)
+                        return
+                else:
+                    return
             mypaintlib.tile_combine(mode, src, dst, dst_has_alpha, opacity)
 
 

commit 0de1ace8b1126f02119d8eee7d451203f627ff99

tile clearing: fix HEAVY_DEBUG assertion
Fixes a C++ assertion accidentally introduced in 6414ab0 which was visible
only when compiled with debug=1.  We use tile_clear() in more
places now, but its action was data-type specific and calling it with the
wrong tile array could cause data corruption (hence the assertion)

* Provide separate tile_clear_…() functions for rgba16 and rgba8 data
* Fixes a potential data corruption with MyPaintSurface.blit_tile_into()
  - was probably never actually provoked in the wild

Minor stuff:

* Headers: remove internal-only …_c() routines from lib/pixops.hpp
* Couch some assertions in #ifdef HEAVY_DEBUG

lib/pixbufsurface.py
@@ -194,7 +194,7 @@ def save_as_png(surface, filename, *rect, **kwargs):
                     except Exception, ex:
                         logger.exception("Failed to blit tile %r of %r",
                                          (tx, ty), surface)
-                        mypaintlib.tile_clear(dst)
+                        mypaintlib.tile_clear_rgba8(dst)
                 if feedback_cb and feedback_counter % TILES_PER_CALLBACK == 0:
                     feedback_cb()
                 feedback_counter += 1

lib/pixops.cpp
@@ -109,7 +109,7 @@ void tile_copy_rgba16_into_rgba16(PyObject * src, PyObject * dst) {
                                  (uint16_t *)PyArray_DATA(dst_arr));
 }
 
-void tile_clear(PyObject * dst) {
+void tile_clear_rgba8(PyObject * dst) {
   PyArrayObject* dst_arr = ((PyArrayObject*)dst);
 
 #ifdef HEAVY_DEBUG
@@ -128,10 +128,31 @@ void tile_clear(PyObject * dst) {
   }
 }
 
+void tile_clear_rgba16(PyObject * dst) {
+  PyArrayObject* dst_arr = ((PyArrayObject*)dst);
+
+#ifdef HEAVY_DEBUG
+  assert(PyArray_Check(dst));
+  assert(PyArray_DIM(dst_arr, 0) == MYPAINT_TILE_SIZE);
+  assert(PyArray_DIM(dst_arr, 1) == MYPAINT_TILE_SIZE);
+  assert(PyArray_TYPE(dst_arr) == NPY_UINT16);
+  assert(PyArray_ISBEHAVED(dst_arr));
+  assert(PyArray_STRIDES(dst_arr)[1] <= 8);
+#endif
+
+  for (int y=0; y<MYPAINT_TILE_SIZE; y++) {
+    uint16_t  * dst_p = (uint16_t*)((char *)PyArray_DATA(dst_arr) + y*PyArray_STRIDES(dst_arr)[0]);
+    memset(dst_p, 0, MYPAINT_TILE_SIZE*PyArray_STRIDES(dst_arr)[1]);
+    dst_p += PyArray_STRIDES(dst_arr)[0];
+  }
+}
+
 
 // Noise used for dithering (the same for each tile).
 
-static const int dithering_noise_size = MYPAINT_TILE_SIZE*MYPAINT_TILE_SIZE*sizeof(uint16_t);
+static const int dithering_noise_per_pixel = 2;
+static const int dithering_noise_size = MYPAINT_TILE_SIZE * MYPAINT_TILE_SIZE
+                                        * dithering_noise_per_pixel;
 static uint16_t dithering_noise[dithering_noise_size];
 static void precalculate_dithering_noise_if_required()
 {
@@ -177,11 +198,14 @@ void tile_convert_rgba16_to_rgba8(PyObject * src, PyObject * dst) {
 #endif
 
   precalculate_dithering_noise_if_required();
-  int noise_idx = 0;
+  const int src_strides = PyArray_STRIDES(src_arr)[0];
+  const int dst_strides = PyArray_STRIDES(dst_arr)[0];
 
+#pragma omp parallel for
   for (int y=0; y<MYPAINT_TILE_SIZE; y++) {
-    uint16_t * src_p = (uint16_t*)((char *)PyArray_DATA(src_arr) + y*PyArray_STRIDES(src_arr)[0]);
-    uint8_t  * dst_p = (uint8_t*)((char *)PyArray_DATA(dst_arr) + y*PyArray_STRIDES(dst_arr)[0]);
+    int noise_idx = y*MYPAINT_TILE_SIZE;
+    uint16_t * src_p = (uint16_t*)((char *)PyArray_DATA(src_arr) + y*src_strides);
+    uint8_t * dst_p = (uint8_t*)((char *)PyArray_DATA(dst_arr) + y*dst_strides);
     for (int x=0; x<MYPAINT_TILE_SIZE; x++) {
       uint32_t r, g, b, a;
       r = *src_p++;
@@ -242,10 +266,11 @@ void tile_convert_rgba16_to_rgba8(PyObject * src, PyObject * dst) {
 
       // Variant C) but with precalculated noise (much faster)
       //
-      const uint32_t add_r = dithering_noise[noise_idx++];
+      const uint32_t add_r = dithering_noise[noise_idx+0];
       const uint32_t add_g = add_r; // hm... do not produce too much color noise
       const uint32_t add_b = add_r;
-      const uint32_t add_a = dithering_noise[noise_idx++];
+      const uint32_t add_a = dithering_noise[noise_idx+1];
+      noise_idx += dithering_noise_per_pixel;
 
 #ifdef HEAVY_DEBUG
       assert(add_a < (1<<15));
@@ -496,12 +521,14 @@ void tile_perceptual_change_strokemap(PyObject * a_obj, PyObject * b_obj, PyObje
   PyArrayObject *b = (PyArrayObject *)b_obj;
   PyArrayObject *res = (PyArrayObject *)res_obj;
 
+#ifdef HEAVY_DEBUG
   assert(PyArray_TYPE(a) == NPY_UINT16);
   assert(PyArray_TYPE(b) == NPY_UINT16);
   assert(PyArray_TYPE(res) == NPY_UINT8);
   assert(PyArray_ISCARRAY(a));
   assert(PyArray_ISCARRAY(b));
   assert(PyArray_ISCARRAY(res));
+#endif
 
   uint16_t * a_p  = (uint16_t*)PyArray_DATA(a);
   uint16_t * b_p  = (uint16_t*)PyArray_DATA(b);

lib/pixops.hpp
@@ -19,9 +19,6 @@
 
 void tile_downscale_rgba16(PyObject *src, PyObject *dst, int dst_x, int dst_y);
 
-void tile_downscale_rgba16_c(const uint16_t *src, int src_strides, uint16_t *dst,
-                             int dst_strides, int dst_x, int dst_y);
-
 
 // Used to e.g. copy the background before starting to composite over it
 //
@@ -29,14 +26,16 @@ void tile_downscale_rgba16_c(const uint16_t *src, int src_strides, uint16_t *dst
 // sadly. The above comment is true when the array is sliced; it's only about
 // two times faster now, in the current use case.
 
-void tile_copy_rgba16_into_rgba16_c(const uint16_t *src, uint16_t *dst);
-
 void tile_copy_rgba16_into_rgba16(PyObject *src, PyObject *dst);
 
 
-// Clear a tile.
+// Clears a tile.
+// This zeroes the alpha channel too, so using it on rgbu data
+// may have unexpected consequences.
 
-void tile_clear(PyObject *dst);
+void tile_clear_rgba16(PyObject *dst);
+
+void tile_clear_rgba8(PyObject *dst);
 
 
 // Converts a 15ish-bit tile array to 8bpp RGBA.
@@ -46,9 +45,6 @@ void tile_convert_rgba16_to_rgba8(PyObject *src, PyObject *dst);
 
 
 // Converts a 15ish-bit tile array to 8bpp RGB ("ignoring" alpha).
-
-void tile_convert_rgbu16_to_rgbu8_c(const uint16_t* src, int src_strides,
-                                    uint8_t* dst, int dst_strides);
 
 void tile_convert_rgbu16_to_rgbu8(PyObject *src, PyObject *dst);
 

lib/tiledsurface.py
@@ -259,24 +259,26 @@ class MyPaintSurface (object):
             return self.mipmap.blit_tile_into(dst, dst_has_alpha, tx, ty, mipmap_level)
 
         assert dst.shape[2] == 4
+        if dst.dtype not in ('uint16', 'uint8'):
+            raise ValueError('Unsupported destination buffer type %r', dst.dtype)
+        dst_is_uint16 = (dst.dtype == 'uint16')
 
         with self.tile_request(tx, ty, readonly=True) as src:
-
             if src is transparent_tile.rgba:
                 #dst[:] = 0 # <-- notably slower than memset()
-                mypaintlib.tile_clear(dst)
+                if dst_is_uint16:
+                    mypaintlib.tile_clear_rgba16(dst)
+                else:
+                    mypaintlib.tile_clear_rgba8(dst)
             else:
-
-                if dst.dtype == 'uint16':
+                if dst_is_uint16:
                     # this will do memcpy, not worth to bother skipping the u channel
                     mypaintlib.tile_copy_rgba16_into_rgba16(src, dst)
-                elif dst.dtype == 'uint8':
+                else:
                     if dst_has_alpha:
                         mypaintlib.tile_convert_rgba16_to_rgba8(src, dst)
                     else:
                         mypaintlib.tile_convert_rgbu16_to_rgbu8(src, dst)
-                else:
-                    raise ValueError, 'Unsupported destination buffer type'
 
     def composite_tile(self, dst, dst_has_alpha, tx, ty, mipmap_level=0,
                        opacity=1.0, mode=mypaintlib.CombineNormal):
@@ -297,7 +299,7 @@ class MyPaintSurface (object):
         if opacity == 0:
             if mode == mypaintlib.CombineDestinationIn:
                 if dst_has_alpha:
-                    mypaintlib.tile_clear(dst)
+                    mypaintlib.tile_clear_rgba16(dst)
                     return
             else:
                 return
@@ -311,7 +313,7 @@ class MyPaintSurface (object):
             if src is transparent_tile.rgba:
                 if mode == mypaintlib.CombineDestinationIn:
                     if dst_has_alpha:
-                        mypaintlib.tile_clear(dst)
+                        mypaintlib.tile_clear_rgba16(dst)
                         return
                 else:
                     return