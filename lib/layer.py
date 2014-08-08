mypaint/mypaint

commit c413a96d7238f0923184f08b6ff4ea6b769e0187

Make "Normal" the default mode for groups
The default mode for groups should be Normal rather than Pass-through.
Pass-through is a bad default for new groups. It's:

* More limiting to the user because its opacity cannot be changed.
* Not called "Normal", which users might justifiably expect! ☺
* Not like what GIMP and Krita do.

Fixes #15.

lib/layer.py
@@ -1012,7 +1012,7 @@ class LayerStack (LayerBase):
     DEFAULT_NAME = _("Group")
 
     PERMITTED_MODES = set(STANDARD_MODES + STACK_MODES)
-    INITIAL_MODE = PASS_THROUGH_MODE
+    INITIAL_MODE = mypaintlib.CombineNormal
 
 
     ## Construction and other lifecycle stuff

commit 275ce0c52fc64aa5d150df01120b1f4131841c48

Fix layer backdrop getter tests
Fix failing doctest for the layer backdrop determination method. Switching
to an isolated "normal" mode for layer groups by default in c413a96 broke
the test.

lib/layer.py
@@ -2787,6 +2787,9 @@ class RootLayerStack (LayerStack):
             ...     ]:
             ...     root.deepinsert(path, layer)
             >>> root.deepget([1,0,0,2]).visible = False
+            >>> root.deepget([1,0,0]).mode = PASS_THROUGH_MODE
+            >>> root.deepget([1,0]).mode = PASS_THROUGH_MODE
+            >>> root.deepget([1]).mode = PASS_THROUGH_MODE
 
         If there are no isolated groups (other than the root stack
         itself), you'll get all the underlying layers including the