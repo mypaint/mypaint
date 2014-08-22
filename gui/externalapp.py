0812b4ce914b3d0202cea4f9689f38606e64304b
Andrew Chadwick <a.t.chadwick@gmail.com>
2014-08-22 18:16:03 +0100

    External layer editing: app chooser dialog; bugfix
    Present a dialog asking which application to launch when attempting to edit
    layer data in an external app.
    
    No longer error out if the app used/chosen will only support URIs. Assume
    the file:// URI it will be launched with will be sufficient.
    
    Fixes #80.

--- gui/externalapp.py
+++ gui/externalapp.py
@@ -12,13 +12,18 @@
 
 ## Imports
 
-from logging import getLogger
-logger = getLogger(__name__)
+import logging
+logger = logging.getLogger(__name__)
 import weakref
 
 from gettext import gettext as _
 
 from gi.repository import Gio
+from gi.repository import Pango
+from gi.repository import Gdk
+from gi.repository import Gtk
+
+from lib.helpers import escape
 
 
 ## UI string consts
@@ -26,10 +31,156 @@ from gi.repository import Gio
 _LAUNCH_SUCCESS_MSG = _(u"Launched {app_name} to edit layer “{layer_name}”")
 _LAUNCH_FAILED_MSG = _(u"Error: failed to launch {app_name} to edit "
                        u"layer “{layer_name}”")
+_LAUNCH_CANCELLED_MSG = _(u"Editing cancelled. You can still edit "
+                          u"“{layer_name}” from the Layers menu.")
 _LAYER_UPDATED_MSG = _(u"Updated layer “{layer_name}” with external edits")
 
 
 ## Class definitions
+
+class OpenWithDialog (Gtk.Dialog):
+    """Choose an app from those recommended for a type"""
+
+    ICON_SIZE = Gtk.IconSize.DIALOG
+    SPECIFIC_FILE_MSG = _(
+        u"MyPaint needs to edit a file of type \u201c{type_name}\u201d "
+        u"({content_type}). What application should it use?"
+        )
+    GENERIC_MSG = _(
+        u"What application should Mypaint use for editing files of "
+        u"type \u201c{type_name}\u201d ({content_type})?"
+        )
+
+    def __init__(self, content_type, specific_file=False):
+        Gtk.Dialog.__init__(self)
+        self.set_title(_("Open With..."))
+        self.add_button(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL)
+        self.add_button(Gtk.STOCK_OK, Gtk.ResponseType.OK)
+        self.set_default_response(Gtk.ResponseType.CANCEL)
+        self.set_response_sensitive(Gtk.ResponseType.OK, False)
+        self.connect("show", self._show_cb)
+
+        content_box = self.get_content_area()
+        content_box.set_border_width(12)
+        content_box.set_spacing(12)
+
+        msg_template = self.GENERIC_MSG
+        if specific_file:
+            msg_template = self.SPECIFIC_FILE_MSG
+        msg_text = msg_template.format(
+            content_type=content_type,
+            type_name=Gio.content_type_get_description(content_type),
+            )
+        msg_label = Gtk.Label(msg_text)
+        msg_label.set_single_line_mode(False)
+        msg_label.set_line_wrap(True)
+        msg_label.set_alignment(0.0, 0.5)
+        content_box.pack_start(msg_label, False, False, 0)
+
+        default_app = Gio.AppInfo.get_default_for_type(content_type, False)
+        default_iter = None
+        app_list_store = Gtk.ListStore(object)
+        for app in Gio.AppInfo.get_all_for_type(content_type):
+            if not app.should_show():
+                continue
+            row_iter = app_list_store.append([app])
+            if default_iter is not None:
+                continue
+            if default_app and Gio.AppInfo.equal(app, default_app):
+                default_iter = row_iter
+
+        # TreeView to show available apps for this content type
+        view = Gtk.TreeView()
+        view.set_model(app_list_store)
+        view.set_headers_clickable(False)
+        view.set_headers_visible(False)
+        view.connect("row-activated", self._row_activated_cb)
+
+        view_scrolls = Gtk.ScrolledWindow()
+        view_scrolls.set_shadow_type(Gtk.ShadowType.IN)
+        view_scrolls.add(view)
+        view_scrolls.set_size_request(375, 225)
+        content_box.pack_start(view_scrolls, True, True, 0)
+
+        # Column 0: application icon
+        cell = Gtk.CellRendererPixbuf()
+        col = Gtk.TreeViewColumn(_("Icon"), cell)
+        col.set_cell_data_func(cell, self._app_icon_datafunc)
+        icon_size_ok, icon_w, icon_h = Gtk.icon_size_lookup(self.ICON_SIZE)
+        if icon_size_ok:
+            col.set_min_width(icon_w)
+        col.set_expand(False)
+        col.set_resizable(False)
+        view.append_column(col)
+
+        # Column 1: application name
+        cell = Gtk.CellRendererText()
+        cell.set_property("ellipsize", Pango.EllipsizeMode.END)
+        cell.set_property("editable", False)
+        col = Gtk.TreeViewColumn(_("Name"), cell)
+        col.set_cell_data_func(cell, self._app_name_datafunc)
+        col.set_expand(True)
+        col.set_min_width(150)
+        view.append_column(col)
+
+        # Selection: mode and initial value
+        selection = view.get_selection()
+        selection.set_mode(Gtk.SelectionMode.SINGLE)
+        if default_iter:
+            selection.select_iter(default_iter)
+            self.set_default_response(Gtk.ResponseType.OK)
+            self.set_response_sensitive(Gtk.ResponseType.OK, True)
+        selection.connect("changed", self._selection_changed_cb)
+
+        # Results go here
+        self.selected_appinfo = default_app   #: The app the user chose
+
+    def _show_cb(self, dialog):
+        content_box = self.get_content_area()
+        content_box.show_all()
+
+    def _app_name_datafunc(self, col, cell, model, it, data):
+        app = model.get_value(it, 0)
+        name = app.get_display_name()
+        desc = app.get_description()
+        if desc is not None:
+            markup_template = "<b>{name}</b>\n{description}"
+        else:
+            markup_template = "<b>{name}</b>\n<i>({description})</i>"
+            desc = _("no description")
+        markup = markup_template.format(
+            name=escape(name),
+            description=escape(desc),
+            )
+        cell.set_property("markup", markup)
+
+    def _app_icon_datafunc(self, col, cell, model, it, data):
+        app = model.get_value(it, 0)
+        icon = app.get_icon()
+        cell.set_property("gicon", icon)
+        cell.set_property("stock-size", self.ICON_SIZE)
+
+    def _row_activated_cb(self, view, treepath, column):
+        model = view.get_model()
+        treeiter = model.get_iter(treepath)
+        if treeiter:
+            appinfo = model.get_value(treeiter, 0)
+            self.selected_appinfo = appinfo
+            self.response(Gtk.ResponseType.OK)
+
+
+    def _selection_changed_cb(self, selection):
+        model, selected_iter = selection.get_selected()
+        if selected_iter:
+            appinfo = model.get_value(selected_iter, 0)
+            self.selected_appinfo = appinfo
+            self.set_response_sensitive(Gtk.ResponseType.OK, True)
+            self.set_default_response(Gtk.ResponseType.OK)
+        else:
+            self.selected_appinfo = None
+            self.set_response_sensitive(Gtk.ResponseType.OK, False)
+            self.set_default_response(Gtk.ResponseType.CANCEL)
+
 
 class LayerEditManager (object):
     """Launch external apps to edit layers, monitoring file changes"""
@@ -49,7 +200,7 @@ class LayerEditManager (object):
 
         :param lib.layer.LayerBase layer: Layer to start editing
 
-        This starts the edit procedure by launching the default
+        This starts the edit procedure by launching a chosen
         application for a tempfile requested from the layer. The file is
         monitored for changes, which are loaded back into the associated
         layer automatically.
@@ -71,22 +222,24 @@ class LayerEditManager (object):
         attr = Gio.FILE_ATTRIBUTE_STANDARD_FAST_CONTENT_TYPE
         file_info = file.query_info(attr, flags, None)
         file_type = file_info.get_attribute_string(attr)
-        appinfo = Gio.AppInfo.get_default_for_type(file_type, False)
-        if not appinfo:
-            logger.error("No default app registered for %r", file_type)
+
+        dialog = OpenWithDialog(file_type, specific_file=True)
+        dialog.set_modal(True)
+        dialog.set_transient_for(self._doc.app.drawWindow)
+        dialog.set_position(Gtk.WindowPosition.CENTER)
+        response = dialog.run()
+        dialog.destroy()
+        if response != Gtk.ResponseType.OK:
+            self._doc.app.show_transient_message(
+                _LAUNCH_CANCELLED_MSG.format(
+                    layer_name=layer.name,
+                ))
             return
+        appinfo = dialog.selected_appinfo
+        assert appinfo is not None
 
         disp = self._doc.tdw.get_display()
         launch_ctx = disp.get_app_launch_context()
-
-        if not appinfo.supports_files():
-            logger.error(
-                "The default handler for %r, %r, only supports "
-                "opening files by URI",
-                appinfo.get_name(),
-                file_path,
-                )
-            return
 
         logger.debug(
             "Launching %r with %r (default app for %r)",
@@ -169,3 +322,17 @@ class LayerEditManager (object):
                 mon.cancel()
                 logger.info("File %r is no longer monitored", file.get_path())
                 self._active_edits[i:i+1] = []
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    dialog = OpenWithDialog("image/svg+xml")
+    #dialog = OpenWithDialog("text/plain")
+    #dialog = OpenWithDialog("image/jpeg")
+    #dialog = OpenWithDialog("application/xml")
+    response = dialog.run()
+    if response == Gtk.ResponseType.OK:
+        app_name = dialog.selected_appinfo.get_name()
+        logger.debug("AppInfo chosen: %r", app_name)
+    else:
+        logger.debug("Dialog was cancelled")
