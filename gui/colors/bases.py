5488aee1c0dddc40156b19ddf98a6075fa7f641d
Andrew Chadwick <a.t.chadwick@gmail.com>
2015-02-24 20:12:38 +0000

    adjuster bases: update to GTK3 syntax, prettify
    * Update gui/colors/bases.py to GTK3 syntax
    * Use a GtkEventBox as the base for cached-bg colour adjusters widgets
     - avoid getting GtkDrawingArea's default background
    * PEP8: tweak comments

--- gui/colors/bases.py
+++ gui/colors/bases.py
@@ -1,5 +1,5 @@
 # This file is part of MyPaint.
-# Copyright (C) 2012 by Andrew Chadwick <andrewc-git@piffle.org>
+# Copyright (C) 2012-2014 by Andrew Chadwick <andrewc-git@piffle.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -7,45 +7,33 @@
 # (at your option) any later version.
 
 
-"""Base widgets for the color selector module.
-"""
+"""Base widgets for the colour selector module"""
 
 import logging
 logger = logging.getLogger(__name__)
 
-import gtk
-from gtk import gdk
+from gi.repository import Gtk
+from gi.repository import Gdk
 import cairo
 
 
-class CachedBgWidgetMixin(object):
-    """Provides widgets with a cached background and bg/fg drawing callbacks.
+class CachedBgWidgetMixin (object):
+    """Provides widgets with a cached background and drawing callbacks
 
-    The background is invalidated whenever the size changes, or when the
-    overridable background-validity method returns something new.
+    The background is invalidated whenever the size changes,
+    or when the overridable background-validity method
+    returns something new.
     """
 
-    __bg = None   #: The cached background: a `cairo.ImageSurface`.
-    __bg_validity = None  #: Validity token for `__bg`.
-
     def __init__(self):
-        """Instantiate, binding events; call *after* `gtk.Widget.__init__()`.
+        """Instantiate, binding events
+
+        This must only be called *after* the base class initializer.
         """
-        try:
-            self.connect("draw", self.__draw_cb)
-        except TypeError:
-            self.connect("expose-event", self.__expose_cb)
+        self.connect("draw", self.__draw_cb)
         self.connect("size-allocate", self.__size_allocate_cb)
         self.__bg = None
         self.__bg_validity = None
-
-    def _get_cairo_context(self, event):
-        """Return a clipped, translated Cairo context for an expose event.
-        """
-        raise NotImplementedError
-
-    def __expose_cb(self, widget, event):
-        self.__draw_cb(widget, self._get_cairo_context(event))
 
     def __draw_cb(self, widget, cr):
         bg_valid = self.__bg is not None
@@ -78,72 +66,57 @@ class CachedBgWidgetMixin(object):
         self.__bg_validity = self.get_background_validity()
 
     def get_background_validity(self):
-        """Return a value which, if changed, means the bg must be redrawn.
+        """Get a validity token for the cached background
 
-        These validity tokens may be anything comparable by ``==``, and should
-        be simple to compute (or at least, simpler than re-rendering the
-        background every time).
+        :return: a value which, if changed, means the bg must be redrawn
+
+        Validity tokens may be anything comparable by ``==``, and
+        should be simpler to compute than re-rendering the background.
         """
         raise NotImplementedError
 
     def render_background_cb(self, cr, w, h, *kw):
-        """Render the background when needed.
-        """
+        """Render the background when needed"""
         raise NotImplementedError
 
     def paint_foreground_cb(self, cr, w, h):
-        """Painting the foreground over the background.
-        """
+        """Paints the foreground over the background """
         raise NotImplementedError
 
     def clear_background(self):
-        """Clears the cached background, making it invalid.
-        """
+        """Clears the cached background, making it invalid"""
         self.__bg = None
         self.__bg_validity = None
         self.queue_draw()
 
 
-class CachedBgDrawingArea (CachedBgWidgetMixin, gtk.DrawingArea):
+class CachedBgDrawingArea (CachedBgWidgetMixin, Gtk.EventBox):
+    """Base class for widgets with cached backgrounds"""
 
     def __init__(self):
-        gtk.DrawingArea.__init__(self)
+        Gtk.EventBox.__init__(self)
+        self.set_app_paintable(True)
         CachedBgWidgetMixin.__init__(self)
 
-    def _get_cairo_context(self, event):
-        """Transformed Cairo context, suitable for windowless impls.
-        """
-        gdk_win = self.get_window()
-        cr = gdk_win.cairo_create()
-        # clip to the expose area, which is always relative to gdk_win
-        cr.rectangle(event.area)
-        cr.clip()
-        if not self.get_has_window():
-            # gdk_win belongs to some parent widget
-            # need to translate cr for correct alignment.
-            x, y, w, h = self.get_allocation()
-            cr.translate(x, y)
-        return cr
 
+class IconRenderable (object):
+    """Mixin for objects that can be rendered as a XDG icons
 
-class IconRenderable(object):
-    """Mixin for anything which can render itself as an XDG icon via Cairo.
-
-    Typically a cached icon file from disk will be quicker and more convenient,
-    and this interface can be used for generating iconic forms for saving to
-    disk.
+    Typically a cached icon file from disk will be quicker
+    and more convenient than rendering the icon
+    each time a tab needs to be drawn, so this mixin
+    can be used for saving icons to disk too.
     """
 
     def render_as_icon(self, cr, size):
-        """Renders as an icon into a Cairo context (unimplemented).
+        """Renders as an icon into a Cairo context (unimplemented)
 
         The icon pixel size, `size`, is one of 48, 32, 24, 22 or 16.
         """
         raise NotImplementedError
 
     def save_icon_tree(self, dir_name, icon_name):
-        """Renders a full set of XDG icons under a given root directory.
-        """
+        """Saves a full set of XDG icons into a given root directory"""
         import os
         dpi = 90.0
         for size in (48, 32, 24, 22, 16, 'scalable'):
