f879520ef70befb82dceeb6696f718867e74ab2a
Andrew Chadwick <a.t.chadwick@gmail.com>
2015-03-29 22:34:45 +0100

    Add wrappers for GLib-style gettext
    GLib defines nicer wrappers around gettext than the standard Python
    gettext library in 2.7. Add a wrapper module allowing us to migrate to it
    in due course.
    
    Complete API: https://git.gnome.org/browse/glib/tree/glib/gi18n-lib.h
    We probably just want C_() as the most Pythonic of these options though,
    with something like the old way of doing _() to support older code.
    And ngettext() of course.
    
    Partly addresses #278.

---
+++ lib/gettext.py
@@ -0,0 +1,50 @@
+# This file is part of MyPaint.
+# Copyright (C) 2015 by Andrew Chadwick <a.t.chadwick@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+from __future__ import absolute_import
+
+"""Function equivalents of (GLib) gettext's C macros.
+
+Recommended usage:
+
+  >>> from lib.gettext import C_
+  >>> from lib.gettext import ngettext
+
+Also supported, but mildly deprecated (consider C_ instead!):
+
+  >>> from lib.gettext import gettext as _
+
+Lots of older code uses ``from gettext import gettext as _``.
+Don't do that in new code: pull in this module as ``lib.gettext``.
+Importing this module should still work from within lib/ if code
+still uses a relative import, however.
+
+"""
+
+from gi.repository import GLib as _GLib
+
+
+# Older code in lib imports these as "from gettext import gettext as _".
+# Pull them in for backwards compat.
+# Might change these to _Glib.dgettext/ngettext instead.
+
+from gettext import gettext
+from gettext import ngettext
+
+
+# Newer code should use C_() even for simple cases, and provide contexts
+# for translators.
+
+def C_(context, msgid):
+    """Translated string with supplied context.
+
+    Convenience wrapper around g_dpgettext2. It's a function not a
+    macro, but use it as if it was a C macro only.
+
+    """
+    return _GLib.dpgettext2(None, context, msgid)
