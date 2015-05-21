206ce16c36eed9210807c68c7065f2a563b79a1e
Andrew Chadwick <a.t.chadwick@gmail.com>
2015-05-21 20:02:45 +0100

    gui/toolbar.py: Update copyright

--- gui/toolbar.py
+++ gui/toolbar.py
@@ -1,5 +1,5 @@
 # This file is part of MyPaint.
-# Copyright (C) 2011-2013 by Andrew Chadwick <a.t.chadwick@gmail.com>
+# Copyright (C) 2011-2015 by Andrew Chadwick <a.t.chadwick@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
