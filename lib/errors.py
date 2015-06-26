595725ff97e4d1306015cf281744b207ef2ec625
Andrew Chadwick <a.t.chadwick@gmail.com>
2015-06-26 22:05:25 +0100

    lib.errors: add OOM wrapper exception class
    Code which is capable of detecting possible OOM conditions should log
    whatever condition or exception it has to hand, then raise a
    lib.errors.AllocationError in its stead with a translated user-facing
    message string.
    
    Code expecting to receive one of these and provide hints to the user should
    also catch MemoryError too, to be on the safe side. AllocationError is for
    when you suspect GI-wrapped code failed due to OOM and spat out a NULL or a
    different exception.

--- lib/errors.py
+++ lib/errors.py
@@ -13,7 +13,7 @@
 class FileHandlingError (Exception):
     """Simple problem loading or saving files; user-facing string
 
-    Covers expected things like missing required ath elements, missing
+    Covers expected things like missing required path elements, missing
     files, unsupported formats etc.
 
     The stringification of a FileHandlingError should always be
@@ -21,8 +21,36 @@ class FileHandlingError (Exception):
     point where it's raised. It may contain diagnostic information, but
     should always be prefixed or suffixed with gloss for ordinary users.
 
+    In general, if one of these is raised as a response to another
+    exception, log that error with (yourmodule.logger.exception()) with
+    programmer-focussed diagnostic info, and favour user-presentable
+    info for the message string.
+
     """
 
     def __init__(self, msg, investigate_dir=None):
         super(FileHandlingError, self).__init__(msg)
         self.investigate_dir = investigate_dir
+
+
+class AllocationError (Exception):
+    """Indicates a failure to construct a required internal object.
+
+    This may be used as a stopgap to cover probable out-of-memory
+    conditions which GI hasn't wrapped as nice Pythonic `MemoryError`s.
+    The GdkPixbuf wrappers are quite prone to this.
+
+    The stringification of an AllocationError should always be
+    presentable to the user directly, and marked for translation at the
+    point where it's raised. It may contain diagnostic information, but
+    should always be prefixed or suffixed with gloss for ordinary users.
+
+    In general, if one of these is raised as a response to another
+    exception, log that error with (yourmodule.logger.exception()) with
+    programmer-focussed diagnostic info, and favour user-presentable
+    info for the message string.
+
+    """
+
+
+
