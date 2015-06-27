682238159968d39c442c1a7f984aa8a0f31818fc
Andrew Chadwick <a.t.chadwick@gmail.com>
2015-06-27 18:19:29 +0100

    Credits: add module copyright

--- gui/meta.py
+++ gui/meta.py
@@ -1,4 +1,13 @@
 # -*- coding: utf8 -*-
+# This file is part of MyPaint.
+# Copyright (C) 2007-2015 by Martin Renold <martinxyz@gmx.ch> and
+# the MyPaint Developement Team
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
 
 """Program meta-information: consts & display.
 
