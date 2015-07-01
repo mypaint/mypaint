382123b3b6309c8bacaeaebc7caaf56171bb117f
Andrew Chadwick <a.t.chadwick@gmail.com>
2015-07-01 19:22:52 +0100

    gui.builderhacks: add usual copyright header

--- gui/builderhacks.py
+++ gui/builderhacks.py
@@ -1,5 +1,12 @@
-#!/usr/bin/python
 # -*- encoding: utf-8 -*-
+# This file is part of MyPaint.
+# Copyright (C) 2013-2015 by Andrew Chadwick <a.t.chadwick@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
 
 """Hacks for loading stuff from GtkBuilder files."""
 
