a3f0ea41822b064bd41fe86b5f34d7f9b9d705eb
Andrew Chadwick <a.t.chadwick@gmail.com>
2015-07-28 18:12:17 +0100

    Update some file header copyright ranges

--- mypaint.py
+++ mypaint.py
@@ -1,5 +1,6 @@
 # This file is part of MyPaint.
-# Copyright (C) 2007-2009 by Martin Renold <martinxyz@gmx.ch>
+# Copyright (C) 2007-2013 by Martin Renold <martinxyz@gmx.ch>
+# Copyright (C) 2013-2015 by the MyPaint Develoment Team.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
