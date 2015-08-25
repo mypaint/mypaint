1ad58e3ea4a020bef80c98e64a139853ac760bef
Andrew Chadwick <a.t.chadwick@gmail.com>
2015-08-25 20:20:18 +0100

    kbm: update copyright

--- gui/keyboard.py
+++ gui/keyboard.py
@@ -1,5 +1,6 @@
 # This file is part of MyPaint.
-# Copyright (C) 2009 by Martin Renold <martinxyz@gmx.ch>
+# Copyright (C) 2009-2013 by Martin Renold <martinxyz@gmx.ch>
+# Copyright (C) 2009-2015 by the MyPaint Development Team
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
