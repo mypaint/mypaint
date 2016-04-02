a0ff39936a824f0677ce1dc075c77f0097edfae5
Andrew Chadwick <a.t.chadwick@gmail.com>
2016-04-02 19:11:25 +0100

    filename_to_unicode: tolerate typelib variations
    Fixes an exception when g_filename_to_utf8() has a more sensible
    wrapping than our old expectation ☺
    
    Addresses mypaint/mypaint#634.
    
    Also remove pointless import from doctest.

--- lib/glib.py
+++ lib/glib.py
@@ -36,7 +36,6 @@ def filename_to_unicode(opsysstring):
     :returns: the converted filename
     :rtype: unicode
 
-    >>> from gi.repository import GLib
     >>> filename_to_unicode('/ascii/only/path')
     u'/ascii/only/path'
     >>> filename_to_unicode(None) is None
@@ -55,7 +54,12 @@ def filename_to_unicode(opsysstring):
     # Other systems are dependent in opaque ways on the environment.
     if not isinstance(opsysstring, str):
         raise TypeError("Argument must be bytes")
-    ustring = GLib.filename_to_utf8(opsysstring, -1, 0, 0)
+    # This function's annotation seems to vary quite a bit.
+    # See https://github.com/mypaint/mypaint/issues/634
+    try:
+        ustring, _, _ = GLib.filename_to_utf8(opsysstring, -1)
+    except TypeError:
+        ustring = GLib.filename_to_utf8(opsysstring, -1, 0, 0)
     if ustring is None:
         raise UnicodeDecodeError(
             "GLib failed to convert %r to a UTF-8 string. "
