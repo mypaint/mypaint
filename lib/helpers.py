4c030fc5f0744652317e5ae311091b6659d3cd11
Andrew Chadwick <a.t.chadwick@gmail.com>
2016-05-20 02:19:20 +0100

    lib.helpers: update copyright header

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,5 +1,6 @@
 # This file is part of MyPaint.
-# Copyright (C) 2007-2008 by Martin Renold <martinxyz@gmx.ch>
+# Copyright (C) 2007-2012 by Martin Renold <martinxyz@gmx.ch>
+# Copyright (C) 2012-2016 by the MyPaint Development Team.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
