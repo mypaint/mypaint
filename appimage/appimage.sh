887bfba3fbadb31575ec50ee4f6565de1fc29fbc
probonopd <probonopd@users.noreply.github.com>
2016-10-30 23:07:33 +0100

    Update appimage.sh

--- appimage/appimage.sh
+++ appimage/appimage.sh
@@ -59,6 +59,10 @@ cp /usr/lib/x86_64-linux-gnu/libg*k-3.so.0 usr/lib/x86_64-linux-gnu/
 # Compile Glib schemas
 ( mkdir -p usr/share/glib-2.0/schemas/ ; cd usr/share/glib-2.0/schemas/ ; glib-compile-schemas . )
 
+GIT_TAGS=$(git describe --tags)
+GLIBC_NEEDED=$(glibc_needed)
+VERSION=$GIT_TAGS.glibc$GLIBC_NEEDED
+
 ########################################################################
 # Copy in the dependencies that cannot be assumed to be available
 # on all target systems
