fb348a801d850b511eed1f6777698c41689d3c06
Alberto Leiva Popper <ydahhrk@gmail.com>
2017-03-02 15:34:44 -0600

    Mode stack: Add layer for brush tools
    Previously, the mode stack would at most pile `OneshotDragMode`s on top
    of whatever tool was previously selected:
    
            <ModeStack [FreehandMode, PanViewMode]>
    
    Now, non-brush tools can be piled on top of brush tools so whenever a
    user swaps brushes, the active tool becomes the last used brush tool:
    
            <ModeStack [FreehandMode, SymmetryEditMode, PanViewMode]>
    
    I think this is a million times more comfortable, because otherwise I
    need to manually swap tools so much. I mean, when I choose a brush, I
    want to paint.
    
    Fixes mypaint/mypaint#530.

--- gui/fill.py
+++ gui/fill.py
@@ -39,7 +39,7 @@ class FloodFillMode (gui.mode.ScrollableModeMixin,
 
     ## Instance vars (and defaults)
 
-    pointer_behavior = gui.mode.Behavior.PAINT_CONSTRAINED
+    pointer_behavior = gui.mode.Behavior.PAINT_NOBRUSH
     scroll_behavior = gui.mode.Behavior.CHANGE_VIEW
 
     _current_cursor = _CURSOR_FILL_PERMITTED
