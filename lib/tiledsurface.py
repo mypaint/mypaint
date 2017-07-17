e5af48b8229928ff8738fad4a2a8ec1209e923ee
Andrew Chadwick <a.t.chadwick@gmail.com>
2017-07-17 20:29:26 +0100

    lib.tiledsurface: update copyright info.
    Review based on commit dates.

--- lib/tiledsurface.py
+++ lib/tiledsurface.py
@@ -1,5 +1,6 @@
 # This file is part of MyPaint.
-# Copyright (C) 2007-2008 by Martin Renold <martinxyz@gmx.ch>
+# Copyright (C) 2009-2017 by the MyPaint Development Team.
+# Copyright (C) 2007-2012 by Martin Renold <martinxyz@gmx.ch>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
