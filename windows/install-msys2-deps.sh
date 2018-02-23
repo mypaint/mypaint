f576e856647a464cbcf70b11deb98da46a028fa5
Andrew Chadwick <a.t.chadwick@gmail.com>
2018-02-23 12:48:32 +0000

    Windows: update the interactive deps helper.
    It was missing a few dependencies, and it lacked explicit user approval.
    Update it to make it clear what is going to be installed, and to use
    the more complete noninteractive helper used by the CI scripting.

--- windows/install-msys2-deps.sh
+++ windows/install-msys2-deps.sh
@@ -1,43 +1,49 @@
 #!/bin/sh
-# Installs runtime dependencies.
+# Interactively install runtime dependencies.
 # User-oriented convenience script for MSYS2 installations.
 # This shouldn't be used as part of anybody's build scripting.
 
-case "$MSYSTEM" in
-    MINGW64)
-        ARCH=x86_64
-        ;;
-    MINGW32)
-        ARCH=i686
-        ;;
-    *)
-        echo >&2 "ERROR: unsupported system."
-        echo >&2 "This script must be run from either the MINGW32 or the MINGW64 "
-        echo >&2 "environment of an MSYS2 installation on Windows."
-        exit 2
-        ;;
-esac
+cat >&2 <<"__END__"
++++ About to install build deps for MyPaint
 
-# Update packages caches
+This script does the following on your behalf. Please read through the
+list and press Return if you're happy. You will be able to uninstall
+everything that gets installed with "pacman" afterwards. Only the files
+in your MSYS2 installation will be affected.
+
+This helper script needs your approval to...
+
+1. Download and install Open Source software from the MSYS2 repositories:
+   - all the runtime dependencies needed by MyPaint
+   - all the necessary developer tools needed to build MyPaint
+2. Download, build, and install Open Source software from GitHub:
+   - libmypaint (MyPaint's C brush engine)
+
++++ Press Return to accept these changes, or Ctrl+C to quit now
+__END__
+
+read OK
+
+echo >&2 "+++ Updating pacman cache, and installing dependencies"
 pacman -Sy --noconfirm
+windows/msys2-build.sh installdeps
 
-# Build and runtime deps, all in one go
-pacman -S --needed --noconfirm \
-    base-devel \
-    git \
-    mingw-w64-$ARCH-toolchain \
-    mingw-w64-$ARCH-pkg-config \
-    mingw-w64-$ARCH-gtk3 \
-    mingw-w64-$ARCH-json-c \
-    mingw-w64-$ARCH-lcms2 \
-    mingw-w64-$ARCH-python2-cairo \
-    mingw-w64-$ARCH-pygobject-devel \
-    mingw-w64-$ARCH-python2-gobject \
-    mingw-w64-$ARCH-python2-numpy \
-    mingw-w64-$ARCH-hicolor-icon-theme \
-    mingw-w64-$ARCH-librsvg \
-    mingw-w64-$ARCH-libmypaint \
-    mingw-w64-$ARCH-setuptools
+echo >&2 "+++ Refreshing loaders.cache to solve missing SVG icons"
+gdk-pixbuf-query-loaders --update-cache
 
-# And for loaders.cache:
-pacman -S --noconfirm mingw-w64-$ARCH-gdk-pixbuf2
+cat >&2 <<__END__
++++ All done!
+
+Dependencies installed for MSYSTEM=$MSYSTEM.
+You should now be able to run the following commands to build and
+test MyPaint from within this folder.
+
+    python setup.py build
+    python setup.py nosetests
+    python setup.py test
+    python setup.py clean --all
+
+    python setup.py demo
+
++++ Happy developing!
+__END__
