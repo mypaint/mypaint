mypaint/mypaint

commit 17f80838e943f163717937d898eb76103f64607d

Begin fixing build and nosetests for Python 3.
Lots of changes needed to get the build and the tests working with Python3.
Not quite there yet, but it's a start.

* New module lib.pycompat for minor PY2/PY3 compatibility fixes.
* All our supported Python versions support b'' and u'', and bytes.
  - No reason not to be explicit.
* Lots of doctest updates to work around u'X' vs 'X' repr()s
* Remove flake8 exception for unicode, xrange.
  - These should be explicitly imported from lib.pycompat now.

lib/tiledsurface.py
@@ -32,6 +32,8 @@ from errors import FileHandlingError
 import lib.fileutils
 import lib.modes
 import lib.feedback
+from lib.pycompat import xrange
+from lib.pycompat import PY3
 
 logger = logging.getLogger(__name__)
 
@@ -424,7 +426,11 @@ class MyPaintSurface (TileAccessible, TileBlittable, TileCompositable):
 
         """
         sshot = _SurfaceSnapshot()
-        for t in self.tiledict.itervalues():
+        if PY3:
+            tiles_iter = self.tiledict.values()
+        else:
+            tiles_iter = self.tiledict.itervalues()
+        for t in tiles_iter:
             t.readonly = True
         sshot.tiledict = self.tiledict.copy()
         return sshot
@@ -440,9 +446,9 @@ class MyPaintSurface (TileAccessible, TileBlittable, TileCompositable):
             # testcase: comparison above (if equal) takes 0.6ms,
             # code below 30ms
             return
-        old = set(self.tiledict.iteritems())
+        old = set(self.tiledict.items())
         self.tiledict = d.copy()
-        new = set(self.tiledict.iteritems())
+        new = set(self.tiledict.items())
         dirty = old.symmetric_difference(new)
         for pos, tile in dirty:
             self._mark_mipmap_dirty(*pos)
@@ -636,7 +642,10 @@ class MyPaintSurface (TileAccessible, TileBlittable, TileCompositable):
         total = 0
         removed = 0
         for surf in self._mipmaps:
-            for pos, data in surf.tiledict.items():
+            tmp_items_list = surf.tiledict.items()
+            if PY3:
+                tmp_items_list = list(tmp_items_list)
+            for pos, data in tmp_items_list:
                 total += 1
                 try:
                     rgba = data.rgba
@@ -859,14 +868,14 @@ class _TiledSurfaceMove (object):
         >>> surf = MyPaintSurface()
         >>> with surf.tile_request(-3, 2, readonly=False) as a:
         ...     a[...] = 1<<15
-        >>> surf.tiledict.keys()
+        >>> list(surf.tiledict.keys())
         [(-3, 2)]
         >>> move = surf.get_move(0, 0, sort=False)
         >>> move.update(N*3, -N*2)
         >>> move.process(n=1)   # single op suffices
         False
         >>> move.cleanup()
-        >>> surf.tiledict.keys()
+        >>> list(surf.tiledict.keys())
         [(0, 0)]
         >>> # Please excuse the doctest for this special case
         >>> # just regression-proofing.
@@ -890,7 +899,8 @@ class _TiledSurfaceMove (object):
         object.__init__(self)
         self.surface = surface
         self.snapshot = surface.save_snapshot()
-        self.chunks = self.snapshot.tiledict.keys()
+        self.chunks = []
+        self.chunks[:] = self.snapshot.tiledict.keys()
         self.sort = sort
         tx = x // N
         ty = y // N
@@ -922,7 +932,7 @@ class _TiledSurfaceMove (object):
         self.written = set()
         # Tile indices to be cleared during processing,
         # unless they've been written to
-        self.blank_queue = self.surface.tiledict.keys()  # fresh!
+        self.blank_queue[:] = self.surface.tiledict.keys()  # fresh!
         if self.sort:
             x, y = self.start_pos
             tx = (x + dx) // N
@@ -1299,7 +1309,11 @@ def flood_fill(src, x, y, color, bbox, tolerance, dst):
 
     # Composite filled tiles into the destination surface
     mode = mypaintlib.CombineNormal
-    for (tx, ty), src_tile in filled.iteritems():
+    if PY3:
+        filled_items = filled.items
+    else:
+        filled_items = filled.iteritems
+    for (tx, ty), src_tile in filled_items:
         with dst.tile_request(tx, ty, readonly=False) as dst_tile:
             mypaintlib.tile_combine(mode, src_tile, dst_tile, True, 1.0)
         dst._mark_mipmap_dirty(tx, ty)
@@ -1336,15 +1350,13 @@ class PNGFileUpdateTask (object):
         tmp_filename = filename + ".tmp"
         if os.path.exists(tmp_filename):
             os.unlink(tmp_filename)
-        tmp_fp = open(tmp_filename, "wb")
         self._png_writer = mypaintlib.ProgressivePNGWriter(
-            tmp_fp,
+            tmp_filename,
             w, h,
             alpha,
             save_srgb_chunks,
         )
         self._tmp_filename = tmp_filename
-        self._tmp_fp = tmp_fp
         # What to write
         self._strips_iter = lib.surface.scanline_strips_iter(
             clone_surface, rect, alpha=alpha,
@@ -1364,7 +1376,6 @@ class PNGFileUpdateTask (object):
             self._png_writer.close()
             self._png_writer = None
             self._strips_iter = None
-            self._tmp_fp.close()
             lib.fileutils.replace(
                 self._tmp_filename,
                 self._final_filename,
@@ -1375,7 +1386,6 @@ class PNGFileUpdateTask (object):
             self._png_writer.close()
             self._png_writer = None
             self._strips_iter = None
-            self._tmp_fp.close()
             if os.path.exists(self._tmp_filename):
                 os.unlink(self._tmp_filename)
             raise

.gitignore
@@ -32,3 +32,4 @@ POTFILES.in
 po/tmp
 build
 flatpak/.flatpak-builder/
+MyPaint.egg-info

gui/brushmanager.py
@@ -34,6 +34,14 @@ from lib.observable import event
 import lib.pixbuf
 from . import drawutils
 import gui.mode
+from lib.pycompat import unicode
+from lib.pycompat import xrange
+from lib.pycompat import PY3
+
+if PY3:
+    import urllib.parse
+else:
+    import urllib
 
 
 ## Public module constants
@@ -67,15 +75,19 @@ logger = logging.getLogger(__name__)
 
 ## Helper functions
 
-
 def _device_name_uuid(device_name):
     """Return UUID5 string for a given device name
 
-    >>> _device_name_uuid('Wacom Intuos5 touch S Pen stylus')
-    'e97830e9-f9f9-50a5-8fff-68bead1a7021'
+    >>> result = _device_name_uuid(u'Wacom Intuos5 touch S Pen stylus')
+    >>> result == u'e97830e9-f9f9-50a5-8fff-68bead1a7021'
+    True
+    >>> type(result) == type(u'')
+    True
+
     """
-    u8bytes = unicode(device_name).encode('utf-8')
-    return str(uuid.uuid5(_DEVICE_NAME_NAMESPACE, u8bytes))
+    if not PY3:
+        device_name = unicode(device_name).encode('utf-8')
+    return unicode(uuid.uuid5(_DEVICE_NAME_NAMESPACE, device_name))
 
 
 def _quote_device_name(device_name):
@@ -84,17 +96,29 @@ def _quote_device_name(device_name):
     Quotes an arbitrary device name for use as the basename of a
     device-specific brush.
 
-        >>> _quote_device_name(u'Heavy Metal Umlaut D\u00ebvice')
-        'Heavy+Metal+Umlaut+D%C3%ABvice'
-        >>> _quote_device_name(u'unsafe/device\u005Cname') # backslash
-        'unsafe%2Fdevice%5Cname'
+    >>> result = _quote_device_name(u'Heavy Metal Umlaut D\u00ebvice')
+    >>> result == 'Heavy+Metal+Umlaut+D%C3%ABvice'
+    True
+    >>> type(result) == type(u'')
+    True
+    >>> result = _quote_device_name(u'unsafe/device\\\\name')
+    >>> result == 'unsafe%2Fdevice%5Cname'
+    True
+    >>> type(result) == type(u'')
+    True
 
     Hopefully this is OK for Windows, UNIX and Mac OS X names.
     """
     device_name = unicode(device_name)
-    u8bytes = device_name.encode("utf-8")
-    quoted = urllib.quote_plus(u8bytes, safe='')
-    return quoted
+    if PY3:
+        quoted = urllib.parse.quote_plus(
+            device_name, safe='',
+            encoding="utf-8",
+        )
+    else:
+        u8bytes = device_name.encode("utf-8")
+        quoted = urllib.quote_plus(u8bytes, safe='')
+    return unicode(quoted)
 
 
 def translate_group_name(name):

gui/colors/adjbases.py
@@ -37,6 +37,7 @@ from lib.palette import Palette
 from lib.observable import event
 import gui.dialogs
 import gui.uicolor
+from lib.pycompat import xrange
 
 logger = logging.getLogger(__name__)
 

gui/colors/hcywheel.py
@@ -39,6 +39,8 @@ from .paletteview import palette_load_via_dialog
 from .paletteview import palette_save_via_dialog
 from lib.gettext import C_
 
+from lib.pycompat import xrange
+
 
 PREFS_MASK_KEY = "colors.hcywheel.mask.gamuts"
 PREFS_ACTIVE_KEY = "colors.hcywheel.mask.active"

gui/colors/hsvcube.py
@@ -31,6 +31,8 @@ from .uimisc import borderless_button
 from .uimisc import PRIMARY_ADJUSTERS_MIN_WIDTH
 from .uimisc import PRIMARY_ADJUSTERS_MIN_HEIGHT
 
+from lib.pycompat import xrange
+
 
 class HSVCubePage (CombinedAdjusterPage):
     """Slice+depth view through an HSV cube: page for `CombinedAdjuster`.

gui/colors/hsvsquare.py
@@ -28,6 +28,8 @@ from .adjbases import IconRenderableColorAdjusterWidget
 from .adjbases import HueSaturationWheelAdjuster
 from .combined import CombinedAdjusterPage
 
+from lib.pycompat import xrange
+
 
 class HSVSquarePage (CombinedAdjusterPage, IconRenderable):
     """Hue ring and Sat+Val square: page for `CombinedAdjuster`."""

gui/colors/paletteview.py
@@ -41,6 +41,8 @@ from .adjbases import ColorManager
 from .adjbases import DATAPATH_PALETTES_SUBDIR
 from .combined import CombinedAdjusterPage
 
+from lib.pycompat import unicode
+
 
 logger = logging.getLogger(__name__)
 

commit a0129f440cefcfd831e548043f3e57860d8b9651

Fix a regression in lib.tiledsurface.flood_fill()
Regression was introduced in 17f80838e943f163717937d898eb76103f64607d.

lib/tiledsurface.py
@@ -1340,9 +1340,9 @@ def flood_fill(src, x, y, color, bbox, tolerance, dst):
     # Composite filled tiles into the destination surface
     mode = mypaintlib.CombineNormal
     if PY3:
-        filled_items = filled.items
+        filled_items = filled.items()
     else:
-        filled_items = filled.iteritems
+        filled_items = filled.iteritems()
     for (tx, ty), src_tile in filled_items:
         with dst.tile_request(tx, ty, readonly=False) as dst_tile:
             mypaintlib.tile_combine(mode, src_tile, dst_tile, True, 1.0)