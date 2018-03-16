9a063b877e09bcd57ebb3b0fed743d5497fc0197
Andrew Chadwick <a.t.chadwick@gmail.com>
2018-03-16 15:10:40 +0000

    Add some flake8 violations for testing.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,3 +1,9 @@
 
 from __future__ import division, print_function
 
+
+def myfunc(a):
+    """This newly-added function contains several flake8 style violations. In particular, E501, E225, and F821 (twice, or once, depending), E265, and W291. It's only here for testing out Hound-CI."""
+    #You shouldn't write code looking like this.   
+    a+=b
+    return unicode(a)
