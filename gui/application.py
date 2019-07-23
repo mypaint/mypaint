eaf3846c2e7de2c67ed1b49b26d16be174d9434b
Jesper Lloyd <jpl.lloyd@gmail.com>
2019-07-23 10:54:27 +0200

    Refactor: reorder gettext initialization
    Refactor config file loading and locale path fetching
    to read user settings prior to gettext initialization.
    
    Factor out command line arg. parsing for readability.
    Move gettext initialization function to new module.
    
    Split out the default-config definition and the loading
    of user settings from main.main() to its own module.
    
    Move the gettext initialization to just before
    the application is initialized.

--- gui/application.py
+++ gui/application.py
@@ -88,6 +88,7 @@ import lib.glib
 import gui.cursor
 import lib.fileutils
 import gui.picker
+import gui.userconfig
 import gui.factoryaction  # registration only
 import gui.autorecover
 import lib.xml
@@ -489,120 +490,10 @@ class Application (object):
         Uses defaults if not explicitly configured.
 
         """
-        def get_json_config():
-            settingspath = join(self.user_confpath, u'settings.json')
-            logger.debug("Reading app settings from %r", settingspath)
-            with open(settingspath, "rb") as fp:
-                jsonstr = fp.read()
-            try:
-                # Py3: settings.json has always been UTF-8 even in Py2.
-                #
-                # The Travis-CI json.loads() from Python 3.4.0 needs
-                # unicode strings, always. Later and earlier versions,
-                # including Py2 do not need that, if bytes are UTF-8.
-                jsonstr = jsonstr.decode("utf-8")
-                return json.loads(jsonstr)
-            except Exception as e:
-                logger.warning("settings.json: %s", str(e))
-                logger.warning("Failed to load settings: using defaults")
-                return {}
-        if sys.platform == 'win32':
-            ud_docs = lib.glib.get_user_special_dir(
-                GLib.UserDirectory.DIRECTORY_DOCUMENTS,
-            )
-            scrappre = os.path.join(ud_docs, u'MyPaint', u'scrap')
-        else:
-            scrappre = u'~/MyPaint/scrap'
-        default_config = {
-            'saving.scrap_prefix': scrappre,
-            'input.device_mode': 'screen',
-            'input.global_pressure_mapping': [(0.0, 1.0), (1.0, 0.0)],
-            'input.use_barrel_rotation': True,
-            'input.barrel_rotation_subtract_ascension': True,
-            'input.barrel_rotation_offset': 0.5,
-            'view.default_zoom': 1.0,
-            'view.real_alpha_checks': True,
-            'ui.hide_menubar_in_fullscreen': True,
-            'ui.hide_toolbar_in_fullscreen': True,
-            'ui.hide_subwindows_in_fullscreen': True,
-            'ui.parts': dict(main_toolbar=True, menubar=True),
-            'ui.feedback.scale': False,
-            'ui.feedback.last_pos': False,
-            'ui.toolbar_items': dict(
-                toolbar1_file=True,
-                toolbar1_scrap=False,
-                toolbar1_edit=True,
-                toolbar1_blendmodes=False,
-                toolbar1_linemodes=True,
-                toolbar1_view_modes=True,
-                toolbar1_view_manips=False,
-                toolbar1_view_resets=True,
-            ),
-            'ui.toolbar_icon_size': 'large',
-            'ui.dark_theme_variant': True,
-            'ui.rendered_tile_cache_size': 16384,
-            'saving.default_format': 'openraster',
-            'brushmanager.selected_brush': None,
-            'brushmanager.selected_groups': [],
-            'frame.color_rgba': (0.12, 0.12, 0.12, 0.92),
-            'misc.context_restores_color': True,
-
-            'document.autosave_backups': True,
-            'document.autosave_interval': 10,
-
-            # configurable EOTF.  Set to 1.0 for legacy non-linear behaviour
-            'display.colorspace_EOTF': 2.2,
-            'display.colorspace': "srgb",
-            # sRGB is a good default even for OS X since v10.6 / Snow
-            # Leopard: http://support.apple.com/en-us/HT3712.
-            # Version 10.6 was released in September 2009.
-
-            "scratchpad.last_opened_scratchpad": "",
-
-            # Initial main window positions
-            "workspace.layout": {
-                "position": dict(x=50, y=32, w=-50, h=-100),
-                "autohide": True,
-            },
-
-            # Linux defaults.
-            # Alt is the normal window resizing/moving key these days,
-            # so provide a Ctrl-based equivalent for all alt actions.
-            'input.button_mapping': {
-                # Note that space is treated as a fake Button2
-                # It is time to free up the modifiers and Button1
-                # '<Shift>Button1': 'StraightMode',
-                # '<Control>Button1': 'ColorPickMode',
-                # '<Alt>Button1': 'ColorPickMode',
-                'Button2': 'PanViewMode',
-                '<Shift>Button2': 'RotateViewMode',
-                '<Control>Button2': 'ZoomViewMode',
-                '<Alt>Button2': 'ZoomViewMode',
-                '<Control><Shift>Button2': 'FrameEditMode',
-                '<Alt><Shift>Button2': 'FrameEditMode',
-                'Button3': 'ShowPopupMenu',
-            },
-        }
-        if sys.platform == 'win32':
-            # The Linux wacom driver inverts the button numbers of the
-            # pen flip button, because middle-click is the more useful
-            # action on Linux. However one of the two buttons is often
-            # accidentally hit with the thumb while painting. We want
-            # to assign panning to this button by default.
-            linux_mapping = default_config["input.button_mapping"]
-            default_config["input.button_mapping"] = {}
-            for bp, actname in linux_mapping.items():
-                bp = bp.replace("Button2", "ButtonTMP")
-                bp = bp.replace("Button3", "Button2")
-                bp = bp.replace("ButtonTMP", "Button3")
-                default_config["input.button_mapping"][bp] = actname
-
+        default_config = gui.userconfig.default_configuration()
         self.preferences.clear()
         self.preferences.update(default_config.copy())
-        try:
-            user_config = get_json_config()
-        except IOError:
-            user_config = {}
+        user_config = gui.userconfig.get_json_config(self.user_confpath)
         self.preferences.update(user_config)
         key = "input.button_mapping"
         if 'ColorPickerPopup' in self.preferences[key].values():
---
+++ lib/gettext_setup.py
@@ -0,0 +1,198 @@
+# This file is part of MyPaint.
+# Copyright (C) 2007-2013 by Martin Renold <martinxyz@gmx.ch>
+# Copyright (C) 2013-2019 by the MyPaint Development Team.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+from __future__ import absolute_import
+
+import os
+import sys
+import logging
+# This imports the global gettext package, not lib/gettext
+import gettext
+
+logger = logging.getLogger(__file__)
+
+
+def init_gettext(localepath):
+    """Initialize locales and gettext.
+
+    This must be done before importing any translated python modules
+    (to get global strings translated, especially brushsettings.py).
+
+    """
+
+    import locale
+    import lib.i18n
+
+    # Required in Windows for the "Region and Language" settings
+    # to take effect.
+    lib.i18n.set_i18n_envvars()
+    lib.i18n.fixup_i18n_envvars()
+
+    # Internationalization
+    # Source of many a problem down the line, so lotsa debugging here.
+    logger.debug("localepath: %r", localepath)
+    logger.debug("getdefaultlocale(): %r", lib.i18n.getdefaultlocale())
+
+    # Set the user's preferred locale.
+    # https://docs.python.org/2/library/locale.html
+    # Required in Windows for the "Region and Language" settings
+    # to take effect.
+    try:
+        setlocale_result = locale.setlocale(locale.LC_ALL, '')
+    except locale.Error:
+        logger.exception("setlocale(LC_ALL, '') failed")
+    else:
+        logger.debug("setlocale(LC_ALL, ''): %r", setlocale_result)
+
+    # More debugging: show the state after setlocale().
+    logger.debug(
+        "getpreferredencoding(): %r",
+        locale.getpreferredencoding(do_setlocale=False),
+    )
+    locale_categories = [
+        s for s in dir(locale)
+        if s.startswith("LC_") and s != "LC_ALL"
+    ]
+    for category in sorted(locale_categories):
+        logger.debug(
+            "getlocale(%s): %r",
+            category,
+            locale.getlocale(getattr(locale, category)),
+        )
+
+    # Low-level bindtextdomain with paths.
+    # This is still required to hook GtkBuilder up with translated
+    # strings; the gettext() way doesn't cut it for external stuff
+    # yanked in over GI.
+    # https://bugzilla.gnome.org/show_bug.cgi?id=574520#c26
+    bindtextdomain = None
+    bind_textdomain_codeset = None
+    textdomain = None
+
+    # Try the POSIX/Linux way first.
+    try:
+        bindtextdomain = locale.bindtextdomain
+        bind_textdomain_codeset = locale.bind_textdomain_codeset
+        textdomain = locale.textdomain
+    except AttributeError:
+        logger.warning(
+            "No bindtextdomain builtins found in module 'locale'."
+        )
+        logger.info(
+            "Trying platform-specific fallback hacks to find "
+            "bindtextdomain funcs.",
+        )
+        # Windows Python binaries tend not to expose bindtextdomain and
+        # its buddies anywhere they can be called.
+        if sys.platform == 'win32':
+            libintl = None
+            import ctypes
+            libnames = [
+                'libintl-8.dll',  # native for MSYS2's MINGW32
+                'libintl.dll',  # no known cases, but a potential fallback
+                'intl.dll',  # some old recipes off the internet
+            ]
+            for libname in libnames:
+                try:
+                    libintl = ctypes.cdll.LoadLibrary(libname)
+                    bindtextdomain = libintl.bindtextdomain
+                    bindtextdomain.argtypes = (
+                        ctypes.c_char_p,
+                        ctypes.c_char_p,
+                    )
+                    bindtextdomain.restype = ctypes.c_char_p
+                    bind_textdomain_codeset = libintl.bind_textdomain_codeset
+                    bind_textdomain_codeset.argtypes = (
+                        ctypes.c_char_p,
+                        ctypes.c_char_p,
+                    )
+                    bind_textdomain_codeset.restype = ctypes.c_char_p
+                    textdomain = libintl.textdomain
+                    textdomain.argtypes = (
+                        ctypes.c_char_p,
+                    )
+                    textdomain.restype = ctypes.c_char_p
+                except Exception:
+                    logger.exception(
+                        "Windows: attempt to load bindtextdomain funcs "
+                        "from %r failed (ctypes)",
+                        libname,
+                    )
+                else:
+                    logger.info(
+                        "Windows: found working bindtextdomain funcs "
+                        "in %r (ctypes)",
+                        libname,
+                    )
+                    break
+        else:
+            logger.error(
+                "No platform-specific fallback for locating bindtextdomain "
+                "is known for %r",
+                sys.platform,
+            )
+
+    # Bind text domains, i.e. tell libintl+GtkBuilder and Python's where
+    # to find message catalogs containing translations.
+    textdomains = [
+        ("mypaint", localepath),
+        # Open question: do we need to bind libmypaint's stuff here too,
+        # now that we have gone sharedlib? It seems to work correctly
+        # under Linux from Python code without an explicit pile of binds.
+        # ("libmypaint", localepath_brushlib),
+    ]
+    defaultdom = "mypaint"
+    codeset = "UTF-8"
+    for dom, path in textdomains:
+        # Some people choose not to install any translation files.
+        if not os.path.isdir(path):
+            logger.warning(
+                "No translations for %s. Missing locale dir %r.",
+                dom, path,
+            )
+            continue
+        # Only call the C library gettext setup funcs if there's a
+        # complete set from the same source.
+        # Required for translatable strings in GtkBuilder XML
+        # to be translated.
+        if bindtextdomain and bind_textdomain_codeset and textdomain:
+            assert os.path.exists(path)
+            assert os.path.isdir(path)
+            if sys.platform == 'win32':
+                p = bindtextdomain(dom.encode('utf-8'), path.encode('utf-8'))
+                c = bind_textdomain_codeset(
+                    dom.encode('utf-8'), codeset.encode('utf-8')
+                )
+            else:
+                p = bindtextdomain(dom, path)
+                c = bind_textdomain_codeset(dom, codeset)
+            logger.debug("C bindtextdomain(%r, %r): %r", dom, path, p)
+            logger.debug(
+                "C bind_textdomain_codeset(%r, %r): %r",
+                dom, codeset, c,
+            )
+        # Call the implementations in Python's standard gettext module
+        # too.  This has proper cross-platform support, but it only
+        # initializes the native Python "gettext" module.
+        # Required for marked strings in Python source to be translated.
+        # See http://docs.python.org/release/2.7/library/locale.html
+        p = gettext.bindtextdomain(dom, path)
+        c = gettext.bind_textdomain_codeset(dom, codeset)
+        logger.debug("Python bindtextdomain(%r, %r): %r", dom, path, p)
+        logger.debug(
+            "Python bind_textdomain_codeset(%r, %r): %r",
+            dom, codeset, c,
+        )
+    if bindtextdomain and bind_textdomain_codeset and textdomain:
+        if sys.platform == 'win32':
+            d = textdomain(defaultdom.encode('utf-8'))
+        else:
+            d = textdomain(defaultdom)
+        logger.debug("C textdomain(%r): %r", defaultdom, d)
+    d = gettext.textdomain(defaultdom)
+    logger.debug("Python textdomain(%r): %r", defaultdom, d)
