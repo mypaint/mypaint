b665173985de3f0c05af9db88b1e24cb4357fbc2
Jesper Lloyd <jpl.lloyd@gmail.com>
2020-03-12 21:06:34 +0100

    svg icons: fix xlink namespace not being specified
    Export script adjusted and all icons re-exported.
    
    librsvg 2.48.0 is stricter about namespaces being specified,
    making this necessary for any of the icons that make use of
    xlink:href (quite a few). Also includes some minor tweaks that
    had not been exported to the actual icon files before.
    
    Thanks to lazka for drawing attention to this.

--- svg/symbolic-icons-extract.py
+++ svg/symbolic-icons-extract.py
@@ -31,6 +31,7 @@ SCOUR = "scour"
 NAMESPACES = {
     "inkscape": "http://www.inkscape.org/namespaces/inkscape",
     "svg": "http://www.w3.org/2000/svg",
+    "xlink": "http://www.w3.org/1999/xlink",
 }
 SUFFIX24 = ":24"
 
